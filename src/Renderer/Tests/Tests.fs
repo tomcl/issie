c68db23a78c087da199aa2e4bce8c11a26363096
Marco Selvatici <marcoselvatici98@gmail.com>
2020-04-25 19:33:37 +0100

    Restructure the tests to make them cleaner.

--- src/Renderer/Tests/Tests.fs
+++ src/Renderer/Tests/Tests.fs
@@ -1,39 +1,20 @@
 open TestLib
 open Expecto
-open Analyser
-open AnalyserTests
 open Simulator
 open SimulatorTests
-
-[<Tests>]
-let portChecksTests =
-    // The idea is that the checks in checkPortTypesAreConsistent should never
-    // fail, since the user should not be able to make them fail.
-    createTestList "portChecks"
-        (fun inp -> match checkPortTypesAreConsistent inp,
-                          checkPortsAreConnectedProperly inp with
-                    | Some err, _ | _, Some err -> Some err
-                    | None, None -> None
-        )
-        testCasesCheckPortsAreConnectedProperly
-
-[<Tests>]
-let cycleChecksTests =
-    createTestList "cycleCheks"
-        (fun (graph, connections) -> analyseGraph graph connections)
-        testCasesAnalyseGraph
 
 [<Tests>]
 let simulatorTests =
     createTestList "simulator"
         (fun (state, inputs) ->
             match prepareSimulation state with
-            | Error e -> failwithf "what? Incorrect diagrams for simulatorTests %A" e
+            | Error e -> Error e
             | Ok simData ->
                 (simData.Graph, inputs) ||> List.fold (fun graph (inputId, bit) ->
                     feedSimulationInput graph inputId bit
                 )
                 |> extractSimulationIOs simData.Outputs
+                |> Ok
         )
         testCasesSimulator
 

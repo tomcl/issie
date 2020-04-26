b68f9769ecf15d24e199272eb9e0fa9fe3ba3e50
Marco Selvatici <marcoselvatici98@gmail.com>
2020-04-26 16:02:52 +0100

    Minor improvements in diagram policies.

--- src/Renderer/Diagram/draw2d_fsharp_interface.js
+++ src/Renderer/Diagram/draw2d_fsharp_interface.js
@@ -54,6 +54,12 @@ function initialiseCanvas (canvas) {
     );
     // Install policy to allow to zoom with: SHIFT + mouse wheel.
     canvas.installEditPolicy(new draw2d.policy.canvas.WheelZoomPolicy());
+    // Make ports visible only when mouse is near them.
+    canvas.installEditPolicy(new draw2d.policy.canvas.CoronaDecorationPolicy({diameterToBeVisible: 70}));
+    // Show guides to align components better.
+    canvas.installEditPolicy(new draw2d.policy.canvas.SnapToGeometryEditPolicy())
+    canvas.installEditPolicy(new draw2d.policy.canvas.SnapToCenterEditPolicy())
+    canvas.installEditPolicy(new draw2d.policy.canvas.SnapToInBetweenEditPolicy())
 }
 
 function clearCanvas (canvas) {

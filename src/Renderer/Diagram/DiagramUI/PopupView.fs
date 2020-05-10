54a236b8d15268e536f96e322ce02a72132c17dc
Marco Selvatici <marcoselvatici98@gmail.com>
2020-05-10 14:59:11 +0100

    Add confirmation popup for delete button.

--- src/Renderer/Diagram/DiagramUI/PopupView.fs
+++ src/Renderer/Diagram/DiagramUI/PopupView.fs
@@ -25,7 +25,7 @@ let stablePopup body =
     ]
 
 let private buildPopup title body foot close =
-    fun model ->
+    fun text ->
         Modal.modal [ Modal.IsActive true ] [
             Modal.background [ Props [ OnClick close ] ] []
             Modal.Card.card [] [
@@ -33,14 +33,15 @@ let private buildPopup title body foot close =
                     Modal.Card.title [] [ str title ]
                     Delete.delete [ Delete.OnClick close ] []
                 ]
-                Modal.Card.body [] [ body model ]
-                Modal.Card.foot [] [ foot model ]
+                Modal.Card.body [] [ body text ]
+                Modal.Card.foot [] [ foot text ]
             ]
         ]
 
-/// Body and foot are functions that take the model and get produced
-/// dynamically. The input to those functions is a string (for example, in a
-/// dialog popup, the string is the current value of the input box.).
+/// Body and foot are functions that take a string of text and produce a
+/// reactElement. The meaning of the input string to those functions is the
+/// content of PopupDialogText (i.e. in a dialog popup, the string is the
+/// current value of the input box.).
 let private dynamicClosablePopup title body foot dispatch =
     buildPopup title body foot (fun _ -> dispatch ClosePopup)
     |> ShowPopup
@@ -49,7 +50,7 @@ let private dynamicClosablePopup title body foot dispatch =
 /// Create a popup and add it to the page. Body and foot are static content.
 /// Can be closed by the ClosePopup message.
 let closablePopup title body foot dispatch =
-    dynamicClosablePopup title (fun _ -> body) (fun _ -> foot) (fun _ -> dispatch ClosePopup)
+    dynamicClosablePopup title (fun _ -> body) (fun _ -> foot) dispatch
 
 /// Get the value for a change event in an input textbox.
 let private getEventValue (event: React.FormEvent) = 
@@ -69,7 +70,7 @@ let dialogPopup title before placeholder buttonText buttonAction isDisabled disp
             ]
     let foot =
         fun dialogText ->
-            Level.level [] [
+            Level.level [ Level.Level.Props [ Style [ Width "100%" ] ] ] [
                 Level.left [] []
                 Level.right [] [
                     Level.item [] [
@@ -88,6 +89,28 @@ let dialogPopup title before placeholder buttonText buttonAction isDisabled disp
                 ]
             ]
     dynamicClosablePopup title body foot dispatch
+
+/// A static confirmation popup.
+let confirmationPopup title body buttonText buttonAction dispatch =
+    let foot =
+        Level.level [ Level.Level.Props [ Style [ Width "100%" ] ] ] [
+            Level.left [] []
+            Level.right [] [
+                Level.item [] [
+                    Button.button [
+                        Button.Color IsLight
+                        Button.OnClick (fun _ -> dispatch ClosePopup)
+                    ] [ str "Cancel" ]
+                ]
+                Level.item [] [
+                    Button.button [
+                        Button.Color IsPrimary
+                        Button.OnClick buttonAction
+                    ] [ str buttonText ]
+                ]
+            ]
+        ]
+    closablePopup title body foot dispatch
 
 /// Display popup, if any is present.
 let viewPopup model =

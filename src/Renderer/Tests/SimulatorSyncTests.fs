3d0f3e1092df52970299ff3dac95be248ae7206d
Marco Selvatici <marcoselvatici98@gmail.com>
2020-05-22 00:56:08 +0200

    More tests for syncrhonous logic.

--- src/Renderer/Tests/SimulatorSyncTests.fs
+++ src/Renderer/Tests/SimulatorSyncTests.fs
@@ -43,11 +43,39 @@ let testCasesSimulatorSync : TestCase list = [
     ]),
     Ok (
         // Tick 0 and 1, out is zero.
-        [ [(ComponentId "a5d52bcd-0a6d-d123-7313-61d0b8b367fd", ComponentLabel "out", 1), [Zero]]
-          [(ComponentId "a5d52bcd-0a6d-d123-7313-61d0b8b367fd", ComponentLabel "out", 1), [Zero]] ]
+        ([(ComponentId "a5d52bcd-0a6d-d123-7313-61d0b8b367fd", ComponentLabel "out", 1), [Zero]]
+         |> List.replicate 2)
         @
         // Then out is 1 three times.
         ([(ComponentId "a5d52bcd-0a6d-d123-7313-61d0b8b367fd", ComponentLabel "out", 1), [One]]
+         |> List.replicate 3)
+    )
+
+    "Three D-flip-flop connected in series, one input, one output (one)",
+    ("main", stateSync3, [], 5, [
+        (ComponentId "3739e54a-fd21-bf60-8fc2-a3d10108c947", [One])
+    ]),
+    Ok (
+        // Tick 0,1 and 2, out is zero.
+        ([(ComponentId "a5d52bcd-0a6d-d123-7313-61d0b8b367fd", ComponentLabel "out", 1), [Zero]]
+         |> List.replicate 3)
+        @
+        // Then out is 1 two times.
+        ([(ComponentId "a5d52bcd-0a6d-d123-7313-61d0b8b367fd", ComponentLabel "out", 1), [One]]
+         |> List.replicate 2)
+    )
+
+    "StateSync1 custom component followed by a DFF (one)",
+    ("main", stateSync4, [stateSync1Dependency], 5, [
+        (ComponentId "03e4c81a-4703-d9f5-dfaf-301de006610f", [One])
+    ]),
+    Ok (
+        // Tick 0 and 1, out is zero.
+        ([(ComponentId "781e7d9d-b18c-d614-dbc0-23bac9e617b7", ComponentLabel "b", 1), [Zero]]
+         |> List.replicate 2)
+         @
+        // Then out is 1 three times.
+        ([(ComponentId "781e7d9d-b18c-d614-dbc0-23bac9e617b7", ComponentLabel "b", 1), [One]]
          |> List.replicate 3)
     )
 ]

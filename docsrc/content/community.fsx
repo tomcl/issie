ae1200d03e9e072ff64765a866ef9a8463319b97
Tom Clarke <tomcl@users.noreply.github.com>
2020-07-18 19:25:36 +0100

    checkpoint new shmew-inspired build, start to change repo name to ISSIE.

---
+++ docsrc/content/community.fsx
@@ -0,0 +1,19 @@
+(**
+# Community #
+
+This presently just has the Announcements page, but more may be added in the future.
+
+## Announcements ##
+
+The announcements tab displays a feed of the latest announcements from the developers. It's not something people
+need to look hard for, but it can make noticing new patches and keep track of the latest changes easier for some.
+
+
+<div class="row">
+  <div class="span1"></div>
+  <div class="span6">
+    <img src="img/communityAnnouncements.png"/>
+  </div>
+</div>
+
+*)
---
+++ docsrc/content/reporting-issues.fsx
@@ -0,0 +1,16 @@
+(**
+# Reporting Issues #
+
+MordhauBuddy logs runtime data to text files that are cleaned up as they 
+grow stale or become too numerous to keep file sizes low. Please include 
+this data as necessary when reporting [issues](https://github.com/Shmew/MordhauBuddy/issues).
+
+If you encounter any issues while using this application you can navigate 
+to the following locations:
+
+ - Windows
+   - `C:\Users\<user>\AppData\Local\mordhau-buddy-updater\logging`
+ - Linux
+   - `~/.local/share/mordhau-buddy/logging`
+
+*)

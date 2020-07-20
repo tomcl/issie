e505e9291161de27256dc0fb7da63d1f62cbffdc
Tom Clarke <tomcl@users.noreply.github.com>
2020-07-20 22:02:50 +0100

    doc updates

--- src/Common/AssemblyInfo.fs
+++ src/Common/AssemblyInfo.fs
@@ -6,8 +6,8 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("Common")>]
 [<assembly: AssemblyProductAttribute("ISSIE")>]
 [<assembly: AssemblyDescriptionAttribute("Schematic editor and Simulator")>]
-[<assembly: AssemblyVersionAttribute("0.4.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.4.0")>]
+[<assembly: AssemblyVersionAttribute("0.5.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.5.0")>]
 [<assembly: AssemblyConfigurationAttribute("Debug")>]
 [<assembly: InternalsVisibleToAttribute("Common.Tests")>]
 do ()
@@ -16,7 +16,7 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Common"
     let [<Literal>] AssemblyProduct = "ISSIE"
     let [<Literal>] AssemblyDescription = "Schematic editor and Simulator"
-    let [<Literal>] AssemblyVersion = "0.4.0"
-    let [<Literal>] AssemblyFileVersion = "0.4.0"
+    let [<Literal>] AssemblyVersion = "0.5.0"
+    let [<Literal>] AssemblyFileVersion = "0.5.0"
     let [<Literal>] AssemblyConfiguration = "Debug"
     let [<Literal>] InternalsVisibleTo = "Common.Tests"

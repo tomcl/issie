1ecfe4c8d702d742663b3d7964986ccfc0020ee7
MarcoSelvatici <marcoselvatici98@gmail.com>
2020-07-26 11:12:05 +0200

    Add precisations to the type descriptions in Types.fs.

--- src/Common/Types.fs
+++ src/Common/Types.fs
@@ -7,11 +7,15 @@ module CommonTypes
 // Specify the position and type of a port in a JSComponent.
 type PortType = Input | Output
 
-/// a componnet I/O
-/// Id is unique per sheet? or component? and used by library?
+/// A component I/O.
+/// Id (like any other Id) is a string generated with 32 random hex charactes,
+/// so it is (practically) globally unique. These Ids are used by the draw2d
+/// library to uniquely refer to ports and components. They are generated via:
+/// http://www.draw2d.org/draw2d_touch/jsdoc_6/#!/api/draw2d.util.UUID.
 /// PortNumber is used to identify which port on a component, contiguous from 0
 /// separately for inputs and outputs.
-/// HostId is????
+/// HostId is the unique Id of the component where the port is. For example,
+/// all three ports on the same And component will have the same HostId.
 type Port = {
     Id : string
     // For example, an And would have input ports 0 and 1, and output port 0.
@@ -21,10 +25,10 @@ type Port = {
     HostId : string
 }
 
-/// Name identified the LoadedComponent used
-/// The labels define legends on symbol
-/// Label strings are unique per CustomComponent
-/// Multiple CustomComponent instances are differentiated by Component data
+/// Name identified the LoadedComponent used.
+/// The labels define legends on symbol.
+/// Label strings are unique per CustomComponent.
+/// Multiple CustomComponent instances are differentiated by Component data.
 type CustomComponentType = {
     Name: string
     // Tuples with (label * connection width).
@@ -59,8 +63,8 @@ type ComponentType =
     | AsyncROM of Memory | ROM of Memory | RAM of Memory // memory is contents
 
 /// JSComponent mapped to F# record.
-/// Id uniquely identifies the component within a sheet and is used by draw2d library
-/// Label is optional descriptor displayed on schematic
+/// Id uniquely identifies the component within a sheet and is used by draw2d library.
+/// Label is optional descriptor displayed on schematic.
 type Component = {
     Id : string
     Type : ComponentType
@@ -80,8 +84,8 @@ type Connection = {
     Vertices : (float * float) list
 }
 
-/// F# data describing the contents of a single schematic sheet
-type CanvasState   = Component list * Connection list
+/// F# data describing the contents of a single schematic sheet.
+type CanvasState = Component list * Connection list
 
 //================================//
 // Componenents loaded from files //
@@ -89,12 +93,17 @@ type CanvasState   = Component list * Connection list
 
 /// Static data describing a schematic sheet loaded as a custom component.
 /// Every sheet is always identified with a file from which it is loaded/saved. 
-/// Name is human readable (and is the filename - with or without extension?) and identifies sheet.
-/// File path is the sheet directory and name (with exptension?)
+/// Name is human readable (and is the filename - without extension) and identifies sheet.
+/// File path is the sheet directory and name (with extension).
 /// InputLabels, OutputLabels are the I/O connections.
-/// The I/O connection integers are bus widths
-/// The I/O connection strings are human readable (are they unique, or is pos in list significant?)
-/// Two instances of a loaded component have the same LoadedComponent data
+/// The I/O connection integers are bus widths.
+/// The I/O connection strings are human readable. The strings are guaranteed
+/// to be unique in the I/O connection list. I.e. An input label may be the same
+/// as an output label, but two input (or output) labels cannot be the same.
+/// The position in the I/O connections list is important as it implicitly
+/// indicates the port number. For example, the first element in the InputLabels
+/// list is related to the Component's Port with PortNumber 0.
+/// Two instances of a loaded component have the same LoadedComponent data.
 type LoadedComponent = {
     Name: string
     FilePath : string
@@ -104,8 +113,8 @@ type LoadedComponent = {
 }
 
 /// Type for an open project which represents a complete design.
-/// ProjectPath is directory containing project files
-/// OpenFileName is name of file from which current schematic sheet is loaded/saved
+/// ProjectPath is directory containing project files.
+/// OpenFileName is name of file from which current schematic sheet is loaded/saved.
 /// LoadedComponents contains the list of schematic sheets, each as a component, one per sheet.
 type Project = {
     ProjectPath : string

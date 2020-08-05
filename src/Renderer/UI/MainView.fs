4ac8acd214a00e9f4c39b399ae28a5da501423aa
ed13santi <37627980+ed13santi@users.noreply.github.com>
2020-08-05 13:30:23 +0200

    extend tabele to the bottom
    This was done by adding an extra row in the table that doesn't contain any waveform

--- src/Renderer/UI/MainView.fs
+++ src/Renderer/UI/MainView.fs
@@ -139,7 +139,7 @@ let private viewRightTab model dispatch =
             viewSimulation model dispatch
         ]
     | WaveSim -> 
-        div [ Style [Width "100%"; MarginLeft "0%"; MarginTop "0px" ] ] 
+        div [ Style [Width "100%"; Height "95%"; MarginLeft "0%"; MarginTop "0px"; OverflowY OverflowOptions.Hidden ] ] 
             (viewWaveSim model dispatch) 
 
 let hideView model dispatch =

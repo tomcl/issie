3bcfb96277945f3cba84bf139c920c84c2827b0a
Tom Clarke <tomcl@users.noreply.github.com>
2021-01-29 10:14:55 +0000

    fix wavesim label bug, bump to 1.1.4

--- src/Renderer/Interface/Version.fs
+++ src/Renderer/Interface/Version.fs
@@ -1,5 +1,5 @@
 ﻿module Version
-let VERSION = [ 1 ; 1 ; 3]
+let VERSION = [ 1 ; 1 ; 4]
 
 // The first 12  white-space separated words in this file must be in the above format - note that spaces are required.
 // This works as valid F# data for displaying the code version and can also be read programmatically from the master branch github file

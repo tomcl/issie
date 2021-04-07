6e48fa5210eaef47177bc56a03a386bf8dfb94e7
JEMerrick <joannaemerrick@gmail.com>
2021-04-07 13:11:16 +0100

    fix tooltips updates

--- src/Renderer/Interface/JSHelpers.fs
+++ src/Renderer/Interface/JSHelpers.fs
@@ -1,6 +1,5 @@
 (*
     JSHelpers.fs
-
     Some JS related utility functions.
 *)
 
@@ -125,97 +124,6 @@ let getColorString (col: CommonTypes.HighLightColor) =
     (sprintf "%A" col).ToLower()
 
 
-/// Properties for react-tippy
-type TooltipsOpts =
-    | Content of string
-    | Animation of string
-    | Arrow of bool
-    | Theme of string
-    | Offset of int * int
-    | HideOnClick of bool
-    | Placement of string
-    | Delay of int * int
-    | ZIndex of int
-    | Interactive of bool
-    | Boundary of string
-    | PopperOptions of obj
-    | MaxWidth of int
-    | AppendTo of HTMLElement
-
-
-let tippyOptsWithContent p c =
-    [
-        Delay (700,100)
-        Placement p
-        Animation "fade"
-        MaxWidth 250
-        Arrow true
-        Theme "material"
-        Interactive true
-        AppendTo Browser.Dom.document.body
-        HideOnClick true
-        Content c
-    ] 
-    |> keyValueList CaseRules.LowerFirst
-
-
-let tippyOpts p =
-    [
-        Delay (700,100)
-        Animation "fade"
-        Placement p
-        MaxWidth 250
-        Arrow true
-        Theme "material"
-        Interactive true
-        AppendTo Browser.Dom.document.body
-        HideOnClick true
-    ] 
-    |> keyValueList CaseRules.LowerFirst
-
-type TippyInstance =
-    abstract setProps: obj -> unit
-    abstract destroy: unit -> unit
-
-type TippySingleton =
-    abstract setProps: obj -> unit
-    abstract destroy: unit -> unit
-    
-/// top-level function from tippy.js to make tooltips
-/// #id will make tooltip on element id
-///
-let tippy' (rClass : string, tippyOpts : obj): TippyInstance array  = importDefault<TippyInstance array> "tippy.js"
-
-let tippyDom (els : Browser.Types.Element array, tippyOpts : obj): TippyInstance array  = importDefault<TippyInstance array> "tippy.js"
-
-
-let createSingleton (tippys: TippyInstance array, props: obj): TippySingleton = import "createSingleton" "tippy.js"
-
-import "*"  "tippy.js/themes/material.css"
-let tippy (tippyOpts: obj) (rClass:string) = tippy'(rClass, tippyOpts)
-let tippy1 rId pos mess = tippy (tippyOptsWithContent pos mess) ("#"+rId) 
-
-let mutable tippyRecord: Map<string,TippyInstance list> = Map.ofList []
-
-let recordTippyInstance (prefix: string) (tip: TippyInstance array) =
-    () 
-
-let tipRef (prefix:string) (pos:string) (text:string) (element: ReactElement) (tip: string) =
-    let ids = prefix + (
-                text 
-                |> Seq.toArray 
-                |> Array.filter System.Char.IsLetterOrDigit
-                |> Array.map string
-                |> String.concat "")               
-    div [Props.Id ids; Props.Ref (fun element -> 
-        // Ref is trigger with null once for stateless element so we need to wait for the second trigger
-        if not (isNull element) && not (element.hasAttribute "data-tippy-content") then 
-            let tippyInst = tippy1 ids pos tip
-            recordTippyInstance prefix tippyInst)           
-        ] [element]
-
-let tipStr (pos:string) (text:string) (tip: string) = tipRef "Str_" pos text (str text) tip
-
 
 
 let testCanvas = Browser.Dom.document.createElement("canvas") :?> HTMLCanvasElement
@@ -224,4 +132,3 @@ let canvasWidthContext = testCanvas.getContext_2d()
 let getTextWidthInPixels(txt:string, font:string) =
    canvasWidthContext.font <- font; // e.g. "16px times new roman";
    canvasWidthContext.measureText(txt).width;
-

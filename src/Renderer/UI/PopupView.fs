fd5a0ecc09c6abbbb3fd34d8d43e0759afe9fa77
JEMerrick <joannaemerrick@gmail.com>
2021-04-07 11:47:07 +0100

    merge popupview

--- src/Renderer/UI/PopupView.fs
+++ src/Renderer/UI/PopupView.fs
@@ -82,12 +82,16 @@ let formatLabelFromType compType (text:string) =
 let formatLabel (comp:Component) (text:string) =
     formatLabelFromType comp.Type (text:string)
 
-let setComponentLabel model (comp:Component) text =
-    let label = formatLabel comp text
-    model.Diagram.EditComponentLabel comp.Id label
+// TODO: removed formatLabel for now
+let setComponentLabel model (sheetDispatch) (comp:Component) text =
+    // let label = formatLabel comp text
+    let label = text // TODO
 
-let setComponentLabelFromText model (comp:Component) text =
-    model.Diagram.EditComponentLabel comp.Id text
+    model.Sheet.ChangeLabel sheetDispatch (ComponentId comp.Id) label
+    //model.Diagram.EditComponentLabel comp.Id label
+
+let setComponentLabelFromText model (comp:Component) text = ()
+    //model.Diagram.EditComponentLabel comp.Id text
 
 //========//
 // Popups //
@@ -366,27 +370,15 @@ let errorNotification text closeMsg =
     fun dispatch ->
         let close = (fun _ -> dispatch closeMsg)
         Notification.notification [
-            Notification.Color  Color.IsDanger
+            Notification.Color IsDanger
             Notification.Props [ notificationStyle ]
         ] [
             Delete.delete [ Delete.OnClick close ] []
             str text
         ]
 
-
-let successNotification text closeMsg =
-    fun dispatch ->
-        let close = (fun _ -> dispatch closeMsg)
-        Notification.notification [
-            Notification.Color  Color.IsSuccess
-            Notification.Props [ notificationStyle ]
-        ] [
-            Delete.delete [ Delete.OnClick close ] []
-            str text
-        ]
 let errorPropsNotification text = errorNotification text ClosePropertiesNotification
 let errorFilesNotification text  = errorNotification text CloseFilesNotification
-let successSimulationNotification text = successNotification text CloseSimulationNotification
 
 let warningNotification text closeMsg =
     fun dispatch ->
@@ -403,7 +395,13 @@ let warningPropsNotification text = warningNotification text ClosePropertiesNoti
 let warningSimNotification text = warningNotification text CloseSimulationNotification
 
 let viewNotifications model dispatch =
-    [ model.Notifications.FromDiagram
+    let sheetNotifications =
+        match model.Sheet.GetNotifications with
+        | Some msg -> Some <| errorNotification msg CloseDiagramNotification
+        | None -> None
+            
+    [ //model.Notifications.FromDiagram
+      sheetNotifications
       model.Notifications.FromSimulation
       model.Notifications.FromFiles
       model.Notifications.FromMemoryEditor

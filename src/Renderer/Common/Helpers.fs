cc08d76fd4e4ff5de92e53fa1447c51bdc045f1f
Tom Clarke <tomcl@users.noreply.github.com>
2021-04-20 18:51:07 +0100

    reduce debug printout

--- src/Renderer/Common/Helpers.fs
+++ src/Renderer/Common/Helpers.fs
@@ -405,7 +405,7 @@ let aggregate(printInterval: float ) =
 
 /// Parameter that controls how recorded times are processed.                     
 let mutable instrumentation: InstrumentationControl = 
-    Off // for aggregate printing
+    aggregate 10000. // for aggregate printing
     // immediate 2. 2. // for immediate printing
     // Off // for no printing
 

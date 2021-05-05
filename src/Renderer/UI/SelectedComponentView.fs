9312f8b313ad555787dada778069c3ef28e09a1e
Tom Clarke <tomcl@users.noreply.github.com>
2021-05-05 15:18:44 +0100

    Bugfix:  Viewer properties

--- src/Renderer/UI/SelectedComponentView.fs
+++ src/Renderer/UI/SelectedComponentView.fs
@@ -114,7 +114,7 @@ let private makeNumberOfBitsField model (comp:Component) text dispatch =
     
     let title, width =
         match comp.Type with
-        | Input w | Output w | NbitsAdder w | NbitsXor w | Register w -> "Number of bits", w
+        | Input w | Output w | NbitsAdder w | NbitsXor w | Register w | Viewer w -> "Number of bits", w
         | SplitWire w -> "Number of bits in the top (LSB) wire", w
         | BusSelection( w, _) -> "Number of bits selected: width", w
         | BusCompare( w, _) -> "Bus width", w

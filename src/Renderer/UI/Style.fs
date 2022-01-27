dd0db9934866671a862aa6a3952ab6d2728c65b3
Aditya Deshpande <aydeshpande20@gmail.com>
2022-01-27 01:17:20 +0000

    Right panel now draggable again for WaveSim

--- src/Renderer/UI/Style.fs
+++ src/Renderer/UI/Style.fs
@@ -22,7 +22,10 @@ let getHeaderHeight =
 let rightSectionWidth (model:Model) =
     match model.RightPaneTabVisible with
     | RightTab.Properties | RightTab.Catalogue -> rightSectionWidthS
-    | RightTab.Simulation ->  rightSectionWidthL
+    | RightTab.Simulation ->  // rightSectionWidthL
+        match model.SimSubTabVisible with
+        | SimSubTab.StepSim | SimSubTab.TruthTable -> rightSectionWidthL
+        | SimSubTab.WaveSim -> sprintf "%dpx" model.WaveSimViewerWidth
 
 let leftSectionWidth model = Style [
     Width (sprintf "calc(100%s - %s - 10px)" "%" (rightSectionWidth model))

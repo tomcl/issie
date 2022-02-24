982b8d05e2502c16f29bd5454800b4eb39f55150
Aditya Deshpande <aydeshpande20@gmail.com>
2022-02-24 22:27:21 +0000

    Added functions for new Table Generation method

--- src/Renderer/Simulator/TruthTableCreate.fs
+++ src/Renderer/Simulator/TruthTableCreate.fs
@@ -2,12 +2,31 @@
 open CommonTypes
 open TimeHelpers
 open SimulatorTypes
+open TruthTableTypes
 open SynchronousUtils
 open NumberHelpers
 
 // REMEMBER TO REMOVE EVENTUALLY!
 let print x =
     printfn "%A" x
+
+let inputValues (tInput: TableInput) =
+    let values1 =
+        ([],tInput.Constraints.Equalities)
+        ||> List.fold (fun rows con ->
+            if rows.Length < tInput.AllowedRowCount then
+                con.Value::rows
+            else
+                rows)
+
+    (values1,tInput.Constraints.Inequalities)
+    ||> List.fold (fun rows con ->
+        if rows.Length < tInput.AllowedRowCount then
+            [con.LowerBound..con.UpperBound] @ rows
+        else
+            rows)
+    |> List.sort
+
 
 let bitCombinations (width: int) (num: int): CellData list =
     convertIntToWireData width num
@@ -48,7 +67,47 @@ let multibitCombinations (widths: int list) =
     combs
     |> List.map (fun l -> l |> List.mapi (fun i n -> 
         convertIntToWireData widths[i] n))
-    
+
+/// Returns a TableInput list with Max and Constrained Row Counts correctly calculated.
+/// Allowed Row Counts will be set to zero.
+let inputsWithCRC (inputs: SimulationIO list) (inputConstraints: ConstraintSet) =
+    let findConstrainedRowCount (tInput: TableInput) =
+    // We asume that all constraints are validated on entry
+    // Therefore, there should be no overlaps
+        match tInput.Constraints with
+        | {Equalities = []; Inequalities = []} -> tInput.MaxRowCount
+        | {Equalities = equ; Inequalities = []} -> equ.Length
+        | {Equalities = equ; Inequalities = ineq} -> 
+            ((0,ineq)
+            ||> List.fold (fun n con -> n + con.Range)) 
+            + equ.Length
+    inputs
+    |> List.map (fun input ->
+        let ti =
+            (input,inputConstraints)
+            ||> initTableInput
+        let crc = findConstrainedRowCount ti
+        {ti with ConstrainedRowCount = crc})
+/// Calculates Allowed Row Counts for each Truth Table input.
+/// Also returns the actual row count for the given inputs and constraints.
+let inputsWithARC (tInputs: TableInput list) limit = 
+    let sortedInputs =
+        tInputs
+        |> List.sortBy (fun ti -> ti.ConstrainedRowCount)
+    (1,sortedInputs)
+    ||> List.mapFold (fun rowcount ti ->
+        let newRowCount = rowcount*ti.ConstrainedRowCount
+        let capacity = limit/rowcount
+        // Case where constrained values of this input can be entirely included
+        if capacity >= ti.ConstrainedRowCount then
+            {ti with AllowedRowCount = ti.ConstrainedRowCount}, newRowCount
+        // Case where constrained values of this input must be truncated
+        else if capacity > 1 then
+            {ti with AllowedRowCount = capacity}, newRowCount
+        else 
+            {ti with AllowedRowCount = 1}, newRowCount
+        )
+
 
 let tableLHS (inputs: SimulationIO list): TruthTableRow list =
     // Bits associated with max rows allowed in Truth Table (2^10 = 1024)
@@ -57,7 +116,7 @@ let tableLHS (inputs: SimulationIO list): TruthTableRow list =
     let widthOneInputs = List.filter (fun (_,_,w) -> w = 1) inputs
     let widthMultiInputs = List.filter (fun (_,_,w) -> w > 1) inputs
 
-    // Restricts number of rows if there are < 12 single-bit inputs
+    // Restricts number of rows if there are > 10 single-bit inputs
     let power =
         if widthOneInputs.Length > maxBits then
             printfn "Truth Table truncated over single-bit inputs"
--- src/Renderer/Simulator/TruthTableTypes.fs
+++ src/Renderer/Simulator/TruthTableTypes.fs
@@ -1,2 +1,88 @@
 ﻿module TruthTableTypes
 
+open SimulatorTypes
+
+//-------------------------------------------------------------------------------------//
+//-----------------------------Truth Table Types---------------------------------------//
+//-------------------------------------------------------------------------------------//
+
+type CellData = 
+    | Bits of wd: WireData
+    | Algebra of var: string
+    | DC //Don't Care
+
+type TruthTableCell = {
+    IO: SimulationIO
+    Data: CellData
+    }
+
+type TruthTableRow = TruthTableCell list
+
+type TruthTable = {
+    TableMap: Map<TruthTableRow,TruthTableRow>
+    XRows: Map<TruthTableRow,TruthTableRow> option
+    }
+
+//-------------------------------------------------------------------------------------//
+//-----------------------------Constraint Types----------------------------------------//
+//-------------------------------------------------------------------------------------//
+
+type ConstraintSet = {
+    Equalities: EqualityConstraint list
+    Inequalities: InequalityConstraint list
+}
+and EqualityConstraint = {
+    IO: SimulationIO
+    Value: int
+}
+and InequalityConstraint = {
+    LowerBound: int
+    IO: SimulationIO
+    UpperBound: int
+    Range: int
+}
+
+let emptyConstraintSet = {
+    Equalities = []
+    Inequalities = []
+}
+
+let makeInequalityConstraint lower io upper = 
+    let range = upper - lower + 1
+    {
+        LowerBound = lower
+        IO = io
+        UpperBound = upper
+        Range = range
+    }
+
+let orderConstraints set =
+    let ordered =
+        set.Inequalities
+        |> List.sortByDescending (fun c -> c.Range)
+    {set with Inequalities = ordered}
+
+type TableInput = {
+    IO: SimulationIO
+    MaxRowCount: int
+    ConstrainedRowCount: int
+    AllowedRowCount: int
+    Constraints: ConstraintSet
+}
+
+let initTableInput (simIO:SimulationIO) (allConstraints: ConstraintSet) =
+    let (_,_,w) = simIO
+    let specificEqualities =
+        allConstraints.Equalities 
+        |> List.filter (fun con -> con.IO = simIO)
+    let specificInequalities =
+        allConstraints.Inequalities
+        |> List.filter (fun con -> con.IO = simIO)
+    {
+        IO = simIO
+        MaxRowCount = int (2.0**w)
+        ConstrainedRowCount = 0
+        AllowedRowCount = 0
+        Constraints = { Equalities = specificEqualities
+                        Inequalities = specificInequalities}
+    }

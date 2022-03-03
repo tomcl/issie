133a860bc66406f0b16387fa245dbd95e76de519
Aditya Deshpande <ad5718@ic.ac.uk>
2022-03-03 22:46:42 +0000

    Added ConstraintsView.fs

---
+++ src/Renderer/UI/ConstraintView.fs
@@ -0,0 +1,29 @@
+﻿//(*
+//    ConstraintView.fs
+//
+//    View functions for constraints and constraint popup
+//*)
+//
+module ConstraintView
+
+open Fulma
+open Fulma.Extensions.Wikiki
+open Fable.React
+open Fable.React.Props
+
+open NumberHelpers
+open Helpers
+open TimeHelpers
+open JSHelpers
+open DrawHelpers
+open DiagramStyle
+open Notifications
+open PopupView
+open MemoryEditorView
+open ModelType
+open CommonTypes
+open SimulatorTypes
+open TruthTableTypes
+open Extractor
+open Simulator
+open TruthTableCreate

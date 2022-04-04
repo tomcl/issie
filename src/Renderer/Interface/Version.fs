52dbb1cecd04679c2b9d825885ee8661aa679e4a
Tom Clarke <tomcl@users.noreply.github.com>
2022-04-04 22:30:09 +0100

    bump to 2.5.0

--- src/Renderer/Interface/Version.fs
+++ src/Renderer/Interface/Version.fs
@@ -1,5 +1,5 @@
 ﻿module Version
-let VERSION = [ 2 ; 4 ; 5 ]
+let VERSION = [ 2 ; 5 ; 0 ]
 
 // The first 12  white-space separated words in this file must be in the above format - note that spaces are required.
 // This works as valid F# data for displaying the code version and can also be read programmatically from the master branch github file

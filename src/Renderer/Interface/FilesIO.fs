470ba86ded95fe19ea2bd8b4924d17cef9c40b08
Jason Zheng <jzz18@ic.ac.uk>
2022-04-22 17:22:52 +1200

    Temporarily change WaveSim functions to return None

--- src/Renderer/Interface/FilesIO.fs
+++ src/Renderer/Interface/FilesIO.fs
@@ -544,7 +544,7 @@ let makeLoadedComponentFromCanvasData (canvas: CanvasState) filePath timeStamp w
         {
             Name = getBaseNameNoExtension filePath
             TimeStamp = timeStamp
-            WaveInfo = waveInfo
+            WaveInfo = None
             FilePath = filePath
             CanvasState = canvas
             InputLabels = inputs

47a7bc51df932fbd3dc3d8bf38f36183e6f4600d
Archontis Pantelopoulos <59971511+apantelopoulos@users.noreply.github.com>
2022-06-14 18:03:37 +0100

    Moved Verilog-related files to Verilog folder

---
+++ src/Renderer/VerilogComponent/NearleyBindings.fs
@@ -0,0 +1,25 @@
+module NearleyBindings
+
+open Fable.Core
+open Fable.Core.JsInterop
+
+
+[<Import("default", from="nearley")>]
+
+[<Emit("import * as verilogGrammar from './verilog.js' ")>]
+let importGrammar : unit = jsNative
+
+[<Emit("import {fix} from './fix.js' ")>]
+let importFix : unit = jsNative
+
+[<Emit("new nearley.Parser(nearley.Grammar.fromCompiled(verilogGrammar))")>]
+let newParser : obj = jsNative
+
+[<Emit("parser.feed($0)")>]
+let feedParser (data:string): unit = jsNative
+
+[<Emit("fix($0)")>]
+let fix (data:string): string = jsNative
+
+[<Emit("JSON.stringify(parser.results[0])")>]
+let parse : string = jsNative

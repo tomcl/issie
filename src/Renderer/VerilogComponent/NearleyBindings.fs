aa2d60805999cc9890c4a19378ed0ad7bc022788
Archontis Pantelopoulos <59971511+apantelopoulos@users.noreply.github.com>
2022-06-15 20:24:01 +0100

    Improved error checking

--- src/Renderer/VerilogComponent/NearleyBindings.fs
+++ src/Renderer/VerilogComponent/NearleyBindings.fs
@@ -9,17 +9,23 @@ open Fable.Core.JsInterop
 [<Emit("import * as verilogGrammar from './verilog.js' ")>]
 let importGrammar : unit = jsNative
 
-[<Emit("import {fix} from './fix.js' ")>]
+[<Emit("import {fix} from './parser.js' ")>]
 let importFix : unit = jsNative
 
-[<Emit("new nearley.Parser(nearley.Grammar.fromCompiled(verilogGrammar))")>]
-let newParser : obj = jsNative
+[<Emit("import {parseFromFile} from './parser.js' ")>]
+let importParser : unit = jsNative
 
-[<Emit("parser.feed($0)")>]
-let feedParser (data:string): unit = jsNative
+// [<Emit("new nearley.Parser(nearley.Grammar.fromCompiled(verilogGrammar))")>]
+// let newParser : obj = jsNative
+
+// [<Emit("parser.feed($0)")>]
+// let feedParser (data:string): unit = jsNative
 
 [<Emit("fix($0)")>]
 let fix (data:string): string = jsNative
 
-[<Emit("JSON.stringify(parser.results[0])")>]
-let parse : string = jsNative
+// [<Emit("JSON.stringify(parser.results[0])")>]
+// let parse : string = jsNative
+
+[<Emit("parseFromFile($0)")>]
+let parseFromFile (source:string): string = jsNative

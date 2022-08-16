20eb135412f4c7cdacb798fbd591e230cf1cf1f7
Archontis Pantelopoulos <59971511+apantelopoulos@users.noreply.github.com>
2022-08-16 15:19:05 +0100

    Change CreateSheet implementation method. Add negation,constants

--- src/Renderer/VerilogComponent/SheetCreator.fs
+++ src/Renderer/VerilogComponent/SheetCreator.fs
@@ -4,6 +4,7 @@ open VerilogTypes
 open CommonTypes
 open DrawHelpers
 open Helpers
+open NumberHelpers
 
 //// HELPERS  ////
 
@@ -12,6 +13,19 @@ type SheetCreationInfo = {
     Connections: Connection list
     UsedNames: Map<string,int>
 }
+
+type Circuit = {
+    Comps: Component list;
+    Conns: Connection list;
+    Out: Port;
+    OutWidth: int
+}
+
+type Slice = {
+    MSB: int;
+    LSB: int;
+}
+
 
 let findEmptyXY (oldMap: (string*Component) list) : XYPos = 
     let size  = List.length oldMap
@@ -75,6 +89,38 @@ let updateSheetInfo (newComps:Component list) (newConns:Connection list) (newMap
         UsedNames = newMap
     }
 
+let extractWidth compType =
+        match compType with
+        |Output width -> width
+        |Input1 (width,_) -> width
+        |_ -> failwithf "Can't happen"
+
+let joinCircuits (inCircuits:Circuit list) (inPorts: Port list) (topCircuit: Circuit) : Circuit = 
+    let conns = 
+        inPorts
+        |> List.mapi (fun index inputPortId ->
+            createConnection inCircuits[index].Out inputPortId
+        )
+        |> List.append topCircuit.Conns
+    
+    let allConns = 
+        inCircuits
+        |> List.collect (fun c -> c.Conns) 
+        |> List.append conns
+        
+    let comps = 
+        inCircuits
+        |> List.collect (fun circ ->
+            circ.Comps    
+        )
+        |> List.append topCircuit.Comps
+    {Comps=comps;Conns=allConns;Out=topCircuit.Out;OutWidth=topCircuit.OutWidth}
+
+
+ 
+
+
+    
 //////////////////////
 
 
@@ -119,7 +165,7 @@ let getIOtoComponentMap (ioDecls:ItemT list) =
 
 
 
-
+////// OLD IMPLEMENTATION ///////
 
 let buildExpressionComponent (rhs:ExpressionT) widthList oldSheetInfo =
     let compId = DrawHelpers.uuid()
@@ -224,6 +270,114 @@ and rhsUpdateCanvasState (expr:ExpressionT) prevComp portNo widthList ioToCompMa
     |_ -> expressionUpdateCanvasState expr prevComp portNo widthList ioToCompMap prevSI
         
 
+let buildExpressionComponent2 (rhs:ExpressionT) width =
+    let compId = DrawHelpers.uuid()
+    let inputPorts = 
+        match rhs.Type with
+        | "bitwise_OR" | "bitwise_XOR" | "bitwise_AND" 
+        | "additive" | "logical_AND" | "logical_OR" ->
+            [createPort compId PortType.Input (Some 0); createPort compId PortType.Input (Some 1)]
+        | "negation" ->
+            [createPort compId PortType.Input (Some 0)]
+        |_ -> [] //TODO
+    let outputPorts =
+        match rhs.Type with
+        | "bitwise_OR" | "bitwise_XOR" | "bitwise_AND" | "negation" ->
+            [createPort compId PortType.Output (Some 0)]
+        |_ -> [] //TODO
+
+    let compType =
+        match rhs.Type with
+        | "negation" -> (NbitsNot width)  
+        | "bitwise_XOR" -> (NbitsXor width)
+        | "bitwise_AND" -> (NbitsAnd width)
+        | "additive" -> (NbitsAdder width)
+        // | "logical_AND" -> 
+        // | "logical_OR" ->
+        |_ -> (NbitsAdder width) //TODO
+    
+    let baseName =  //from getPrefix
+        match rhs.Type with
+        |"bitwise_XOR" -> "NXOR"
+        | "additive" -> "ADD"
+        | "bitwise_AND" -> "AND"
+        | "negation" -> "NOT"
+        |_ -> "TODO"
+
+        
+    let comp = createComponent compId compType baseName inputPorts outputPorts 0. 0. 
+    comp
+
+
+let createPrimaryCircuit (primary:PrimaryT) (ioAndWireToCompMap:Map<string,Component>) =
+        let name = primary.Primary.Name
+        let inputComp = Map.find name ioAndWireToCompMap
+        match Option.isNone primary.BitsStart with
+        |true -> 
+            let width = extractWidth inputComp.Type //TODO
+            {Comps=[];Conns=[];Out=inputComp.OutputPorts[0];OutWidth=width}
+        |false ->
+            let bStart,bEnd = (int (Option.get primary.BitsStart)),(int (Option.get primary.BitsEnd))
+            
+            let lsb,outWidth = bEnd,(bStart-bEnd+1)
+            let busSelCompId = DrawHelpers.uuid()
+            let inputPorts = [createPort busSelCompId PortType.Input (Some 0)]
+            let outputPorts = [createPort busSelCompId PortType.Output (Some 0)]
+
+            let busSelComp = createComponent busSelCompId (BusSelection (outWidth,lsb)) "" inputPorts outputPorts 0. 0.
+
+            let conn = createConnection inputComp.OutputPorts[0] busSelComp.InputPorts[0]     
+            {Comps=[busSelComp];Conns=[conn];Out=busSelComp.OutputPorts[0];OutWidth=outWidth}
+
+
+let createNumberCircuit (number:NumberT) =
+    let width = (Option.get number.Bits) |> int
+    let _base = Option.get number.Base
+    let no = (Option.get number.AllNumber)
+    let text = 
+        match _base with
+        |"'b" -> "0b"+no
+        |"'h" -> "0h"+no
+        |_ -> no
+    let constValue =
+        match NumberHelpers.strToIntCheckWidth width text with
+        |Ok n -> n
+        |Error _ -> failwithf "Shouldn't happen!"
+    
+    let constId = DrawHelpers.uuid()
+    let outputPorts = [createPort constId PortType.Output (Some 0)]
+    let constComp = createComponent constId (Constant1 (width,constValue,text)) "C" [] outputPorts 0. 0.
+    {Comps=[constComp];Conns=[];Out=constComp.OutputPorts[0];OutWidth=width}
+
+let buildUnaryCircuit (unary:UnaryT) ioAndWireToCompMap =
+    let tempPort = createPort "hostId" PortType.Input (Some 0)
+    match unary.Type with
+    |"primary" ->
+        createPrimaryCircuit (Option.get unary.Primary) ioAndWireToCompMap
+    |"number" ->
+        createNumberCircuit (Option.get unary.Number)
+    |"parenthesis" ->
+        {Comps=[];Conns=[];Out=tempPort;OutWidth=1}
+    |"concat" ->
+        {Comps=[];Conns=[];Out=tempPort;OutWidth=1}
+    |_ -> failwithf "Can't happen"
+
+let rec buildExpressionCircuit (expr:ExpressionT) ioAndWireToCompMap = 
+    match expr.Type with
+    |"unary" -> buildUnaryCircuit (Option.get expr.Unary) ioAndWireToCompMap
+    | "conditional_cond" -> failwithf "todo" //TODO 
+    | "negation" ->
+        let (c1:Circuit) = buildUnaryCircuit (Option.get expr.Unary) ioAndWireToCompMap
+        let topComp = buildExpressionComponent2 expr c1.OutWidth
+        let topCircuit = {Comps=[topComp];Conns=[];Out=topComp.OutputPorts[0];OutWidth=c1.OutWidth}
+        joinCircuits [c1] [topComp.InputPorts[0]] topCircuit
+    | _ ->        
+        let (c1:Circuit) = buildExpressionCircuit (Option.get expr.Head) ioAndWireToCompMap 
+        let (c2:Circuit) = buildExpressionCircuit (Option.get expr.Tail) ioAndWireToCompMap 
+        let topComp = buildExpressionComponent2 expr c1.OutWidth
+        let topCircuit = {Comps=[topComp];Conns=[];Out=topComp.OutputPorts[0];OutWidth=c1.OutWidth}
+        joinCircuits [c1;c2] [topComp.InputPorts[0];topComp.InputPorts[1]] topCircuit
+
 
 let buildCanvasStateForAssignment (statement:StatementItemT) (ioToCompMap:Map<string,Component>) (prevSI:SheetCreationInfo) =
     match statement.StatementType with
@@ -241,23 +395,48 @@ let buildCanvasStateForAssignment (statement:StatementItemT) (ioToCompMap:Map<st
     
     |_ -> prevSI //TODO: wires
 
+///////////////////
 
 
 
-//// MAIN FUNCTION ////
+let getWireToCompMap (lhs:AssignmentLHST) ioAndWireToCompMap =
+    let wireLabelId = DrawHelpers.uuid()
+    let name = lhs.Primary.Name
+    let inputPorts = [createPort wireLabelId PortType.Input (Some 0)]
+    let outputPorts = [createPort wireLabelId PortType.Output (Some 0)]
+    let emptyPos = findEmptyXY (ioAndWireToCompMap |> Map.toList)
+    
+    let wireComp = createComponent wireLabelId IOLabel name inputPorts outputPorts emptyPos.X emptyPos.Y
+    Map.add name wireComp ioAndWireToCompMap
+
+
+let collectWiresLHS (assignments:ItemT list) =
+    let wires = assignments |> List.filter (fun item -> (Option.get item.Statement).StatementType = "wire")
+    wires
+    |> List.map (fun item -> (Option.get item.Statement).Assignment.LHS)
+
+//// OLD MAIN FUNCTION ////
 
 let createSheet input = 
     let items = input.Module.ModuleItems.ItemList |> Array.toList
     let ioDecls = items |> List.filter (fun item -> Option.isSome item.IODecl)
     let assignments = items |> List.filter (fun item -> Option.isSome item.Statement) 
-    
-    let ioToCompMap = getIOtoComponentMap ioDecls
-    // printfn "test %A" ioToCompMap
-    let ioComps = ioToCompMap |> Map.toList |> List.map snd
+    let wiresLHS = collectWiresLHS assignments
+
+    let ioToCompMap = getIOtoComponentMap ioDecls    
+
+    let ioAndWireToCompMap =
+        (ioToCompMap,wiresLHS) 
+        ||> List.fold(fun map wire ->
+            getWireToCompMap wire map
+        )
+
+
+    let ioAndWireComps = ioAndWireToCompMap |> Map.toList |> List.map snd
     
     let initialSheetInfo = 
         {
-            Components = ioComps
+            Components = ioAndWireComps
             Connections = []
             UsedNames = Map.empty<string,int> 
         }
@@ -265,8 +444,134 @@ let createSheet input =
     let finalSheetInfo: SheetCreationInfo = 
         (initialSheetInfo,assignments)
         ||> List.fold (fun sheetCreationInfo assignment ->
-            buildCanvasStateForAssignment (Option.get assignment.Statement) ioToCompMap sheetCreationInfo    
+            buildCanvasStateForAssignment (Option.get assignment.Statement) ioAndWireToCompMap sheetCreationInfo    
         )
     
     let finalCanvasState = finalSheetInfo.Components,finalSheetInfo.Connections
+    Helpers.JsonHelpers.stateToJsonString (finalCanvasState, None)
+
+///////////////
+
+let sliceFromBits (lhs:AssignmentLHST) (ioAndWireToCompMap: Map<string,Component>) = 
+    match (Option.isSome lhs.BitsStart) with
+    |true -> 
+        let bStart = (Option.get lhs.BitsStart)
+        let bEnd = (Option.get lhs.BitsEnd)
+        {MSB = (int bStart); LSB =(int bEnd) }
+    |false ->
+        let comp = Map.find lhs.Primary.Name ioAndWireToCompMap
+        let width = extractWidth comp.Type
+        {MSB = (width-1); LSB=0}
+
+
+let rec joinWithMerge (lst:(Circuit*string*Slice) list) = 
+    let extractCircuitAndPortName item =
+        match item with |(x,y,_)->(x,y)
+    
+    let merge2Circuits (c1:Circuit,name:string,slice:Slice) (c2:Circuit,name2:string,slice2:Slice) = 
+        let mergeWiresId = DrawHelpers.uuid();
+        let inputPorts = 
+            [
+                createPort mergeWiresId PortType.Input (Some 0);
+                createPort mergeWiresId PortType.Input (Some 1)
+            ]
+        let outputPorts =
+            [
+                createPort mergeWiresId PortType.Output (Some 0)
+            ]
+
+        let comp = createComponent mergeWiresId MergeWires "" inputPorts outputPorts 0. 0.
+        let topCircuit = {Comps=[comp];Conns=[];Out=outputPorts[0];OutWidth=0}
+        joinCircuits [c1;c2] [inputPorts[0];inputPorts[1]] topCircuit, name, slice
+
+    match List.length lst with 
+    |1 -> lst[0]
+    |2 -> merge2Circuits lst[0] lst[1]
+    |_ ->
+        let _,back = lst |> List.splitAt 2
+        let m1 = merge2Circuits lst[0] lst[1]
+        joinWithMerge (List.append [m1] back)
+
+
+let attachToOutput (ioAndWireToCompMap: Map<string,Component>) (circuit:Circuit,portName:string,slice:Slice) : CanvasState =
+    let outputPortComp = Map.find portName ioAndWireToCompMap
+    let conn = createConnection circuit.Out outputPortComp.InputPorts[0]
+    
+    let allComps = circuit.Comps@[outputPortComp]
+    let allConns = circuit.Conns@[conn]
+    (allComps,allConns)
+
+
+let concatenateCanvasStates (mainCS: CanvasState) (newCS:CanvasState) : CanvasState =
+    ((fst mainCS)@(fst newCS),(snd mainCS)@(snd newCS))
+
+
+let collectInputAndWireComps (ioAndWireToCompMap:Map<string,Component>) =
+    ioAndWireToCompMap
+    |> Map.toList
+    |> List.map snd
+    |> List.filter (fun comp ->
+        match comp.Type with
+        |Input1 (_,_) |IOLabel -> true
+        |_ -> false
+    )
+    
+
+let fixCanvasState (oldCanvasState:CanvasState) =
+    let fixedComps =
+        oldCanvasState
+        |> fst
+        |> List.mapi (fun i comp ->
+            let newLabel = 
+                match comp.Type with
+                |IOLabel |Input1 (_,_)| Output _ -> comp.Label
+                |_ ->
+                    match comp.Label with 
+                    |"" -> "" 
+                    |_ -> comp.Label+(string i)
+            let x,y = (float (i+1)*120.),(float (i+1)*120.)
+            {comp with Label=newLabel;X=x;Y=y}
+        )
+    (fixedComps,snd oldCanvasState)
+    
+
+let createSheet2 input = 
+    let items = input.Module.ModuleItems.ItemList |> Array.toList
+    let ioDecls = items |> List.filter (fun item -> Option.isSome item.IODecl)
+    let assignments = items |> List.filter (fun item -> Option.isSome item.Statement) 
+    let wiresLHS = collectWiresLHS assignments
+
+    let ioToCompMap = getIOtoComponentMap ioDecls    
+
+    let ioAndWireToCompMap =
+        (ioToCompMap,wiresLHS) 
+        ||> List.fold(fun map wire ->
+            getWireToCompMap wire map
+        )
+
+    let ioAndWireComps = ioAndWireToCompMap |> Map.toList |> List.map snd
+
+    let perItemCircuits = 
+        assignments
+        |> List.map (fun item ->
+            let assignment = (Option.get item.Statement).Assignment
+            let circuit = buildExpressionCircuit assignment.RHS ioAndWireToCompMap
+            let outPort = assignment.LHS.Primary.Name
+            let bits = sliceFromBits assignment.LHS ioAndWireToCompMap
+            (circuit,outPort,bits)
+        )
+
+    let finalCanvasState = 
+        perItemCircuits
+        |> List.groupBy (fun (_,portName,_) -> portName)
+        |> List.map (fun (portName,circuits) ->
+            let sorted = List.sortBy (fun (_,_,slice)->slice.MSB) circuits
+            sorted
+            |> joinWithMerge
+            |> attachToOutput ioAndWireToCompMap
+        )
+        |> List.reduce (fun cs1 cs2 -> concatenateCanvasStates cs1 cs2)
+        |> concatenateCanvasStates (collectInputAndWireComps ioAndWireToCompMap,[])
+        |> fixCanvasState
+
     Helpers.JsonHelpers.stateToJsonString (finalCanvasState, None)

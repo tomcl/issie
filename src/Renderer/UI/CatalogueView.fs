cf66b2c09f911b186280f549c698296f7a626a4a
Tom Clarke <tomcl@users.noreply.github.com>
2022-08-27 09:22:37 +0100

    remove redundant Decode4 from catalogue, improve hovers

--- src/Renderer/UI/CatalogueView.fs
+++ src/Renderer/UI/CatalogueView.fs
@@ -674,6 +674,13 @@ let compareModelsApprox (m1:Model) (m2:Model) =
 
 
 let viewCatalogue model dispatch =
+
+        let muxTipMessage (numBusses:string) = $"Selects the one of its {numBusses} input busses numbered by the value of the select input 
+                                to be the output. Adjusts bus width to match"
+
+        let deMuxTipMessage (numBits:string) = $"The output numbered by the binary value 
+        of the {numBits} sel inputs is equal to Data, the others are 0"
+
         let viewCatOfModel = fun model ->                 
             let styles = 
                 match model.Sheet.Action with
@@ -719,17 +726,12 @@ let viewCatalogue model dispatch =
                           catTip1 "Xnor" (fun _ -> createCompStdLabel Xnor model dispatch) "Output is 1 if the two inputs have the same values"]
                     makeMenuGroup
                         "Mux / Demux"
-                        [ catTip1 "Mux2" (fun _ -> createCompStdLabel Mux2 model dispatch) "Selects the one of its two input busses numbered by the value of the select input
-                                                                                to be the output. Adjusts bus width to match."
-                          catTip1 "Mux4" (fun _ -> createCompStdLabel Mux4 model dispatch) "Selects the one of its four input busses numbered by the value of the select input
-                                                                                            to be the output. Adjusts bus width to match."
-                          catTip1 "Mux8" (fun _ -> createCompStdLabel Mux8 model dispatch) "Selects the one of its eight input busses numbered by the value of the select input
-                                                                                            to be the output. Adjusts bus width to match."                                                                  
-                          catTip1 "Demux2" (fun _ -> createCompStdLabel Demux2 model dispatch)  "The output is equal to the input, the other is 0"
-                          catTip1 "Demux4" (fun _ -> createCompStdLabel Demux4 model dispatch)  "The output is equal to the input"
-                          catTip1 "Demux8" (fun _ -> createCompStdLabel Demux8 model dispatch)  "The output is equal to the input"
-                          catTip1 "Decode4" (fun _ -> createCompStdLabel Decode4 model dispatch) "The output numbered by the binary value 
-                                                                                                of the 2 bit sel input is equal to Data, the others are 0"]
+                        [ catTip1 "Mux2" (fun _ -> createCompStdLabel Mux2 model dispatch) <| muxTipMessage "two"
+                          catTip1 "Mux4" (fun _ -> createCompStdLabel Mux4 model dispatch) <| muxTipMessage "four"
+                          catTip1 "Mux8" (fun _ -> createCompStdLabel Mux8 model dispatch) <| muxTipMessage "eight"                                                             
+                          catTip1 "Demux2" (fun _ -> createCompStdLabel Demux2 model dispatch)  <| deMuxTipMessage "two"  
+                          catTip1 "Demux4" (fun _ -> createCompStdLabel Demux4 model dispatch)  <| deMuxTipMessage "four"
+                          catTip1 "Demux8" (fun _ -> createCompStdLabel Demux8 model dispatch)  <| deMuxTipMessage "eight" ]
                     makeMenuGroup
                         "Arithmetic"
                         [ catTip1 "N bits adder" (fun _ -> createNbitsAdderPopup model dispatch) "N bit Binary adder with carry in to bit 0 and carry out from bit N-1"

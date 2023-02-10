e1b1057d34fb0f87f1a89e1079a60826ae30ee14
Tom Clarke <tomcl@users.noreply.github.com>
2023-02-10 23:54:05 +0000

    add TestResize to Edit menu

--- src/Renderer/DrawBlock/SmartSizeSymbol.fs
+++ src/Renderer/DrawBlock/SmartSizeSymbol.fs
@@ -11,25 +11,25 @@ open Symbol
 open Optics
 open Operators
 
-/// To test this, it must be given two symbols interconnected by wires. It then reorders the ports on
-/// symbolToOrder so that the connecting wires do not cross.
-/// Tt should work out the interconnecting wires (wiresToOrder) from 
+/// To test this, it must be given two symbols interconnected by wires. It then resizes symbolToSize
+/// so that the connecting wires are exactly straight
+/// Tt should work out the interconnecting wires (wires) from 
 ////the two symbols, wModel.Wires and sModel.Ports
-/// It will do nothing if symbolToOrder is not a Custom component (which has re-orderable ports).
+/// It will do nothing if symbolToOrder is not a Custom component (which has adjustable size).
 let reSizeSymbol 
     (wModel: BusWireT.Model) 
-    (symbolToOrder: Symbol) 
+    (symbolToSize: Symbol) 
     (otherSymbol: Symbol) 
         : BusWireT.Model =
-    printfn $"ReSizeSymbol: ToOrder:{symbolToOrder.Component.Label}, Other:{otherSymbol.Component.Label}"
+    printfn $"ReSizeSymbol: ToResize:{symbolToSize.Component.Label}, Other:{otherSymbol.Component.Label}"
     let sModel = wModel.Symbol
 
-    let wiresToOrder = [] // replace this with correct wires
+    let wires = [] // replace this with correct wires
 
-    let symbol' = symbolToOrder // no change at the moment
+    let symbol' = symbolToSize // no change at the moment
 
     {wModel with 
-        Wires = wModel.Wires // no change for now, but probably this function should use update wires after reordering.
+        Wires = wModel.Wires // no change for now, but probably this function should use update wires after resizing.
                              // to make that happen the tyest function which calls this would need to provide an updateWire
                              // function to this as a parameter (as was done in Tick3)
         Symbol = {sModel with Symbols = Map.add symbol'.Id symbol' sModel.Symbols}

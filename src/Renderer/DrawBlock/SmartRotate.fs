e5c51309d35e4b1e779c2b73888785d544bd0451
Tom Clarke <tomcl@users.noreply.github.com>
2023-02-11 15:39:23 +0000

    add SmartRotate

---
+++ src/Renderer/DrawBlock/SmartRotate.fs
@@ -0,0 +1,32 @@
+﻿module SmartRotate
+open Elmish
+open Fable.React.Props
+open CommonTypes
+open Fable.React
+open DrawModelType
+open DrawModelType.SymbolT
+open DrawModelType.BusWireT
+open Symbol
+open Optics
+open Operators
+
+(*
+    HLP23 this is a placeholder module for some work that can be done in the individual or team phase
+    but has not been given a "starter" by me. however it is pretty easy to get started on it.
+
+    Currently if multiple symbols are selected and rotated, each symbol will rotate, but the positions
+    of teh symbols will stay fixed. The desired function for the entire block of symbols to rotate,
+    applying 2-D rotation 90 degree rotation and flipping to the symbol positions about the centre of 
+    the block of selected symbols.
+
+    This operation has "simple" and "better" implementations, like all the initial tasks:
+    1. Rotate all symbols and wires exact - do not allow custom components
+    2. Rotate all symbols, allow custom components, CC wires will change due to CC shape changes,
+      and must be partial autorouted.
+    3. Allow scaling as well as rotation (autoroute wires since components will not scale and therefore
+      exact wire shape will chnage). thsi operation can include Custom components since all wires are
+      autorouted anyway.
+    Driver test code can easily be adapted from existing Smart module Test menu items. Menu commands 
+    which operate on selected symbols - the tests will be more or less how this operation is actually used).
+*)
+

a1225183db79db5396c51d22af419ee242b0d61d
Derek Lai <medereklai@gmail.com>
2023-02-17 22:03:50 +0000

    Implemented WireId Retrieval for Wires in Channel

--- src/Renderer/DrawBlock/SmartChannel.fs
+++ src/Renderer/DrawBlock/SmartChannel.fs
@@ -45,41 +45,37 @@ let overlap1D ((a1, a2): float * float) ((b1, b2): float * float): bool =
     a_max >= b_min && b_max >= a_min
 
 let overlap2D ((a1, a2): XYPos * XYPos) ((b1, b2): XYPos * XYPos): bool =
-    overlap1D (a1.X, a2.X) (b1.X, b2.X) &&
-    overlap1D (a1.Y, a2.Y) (b1.Y, b2.Y)
+    (overlap1D (a1.X, a2.X) (b1.X, b2.X)) &&
+    (overlap1D (a1.Y, a2.Y) (b1.Y, b2.Y))
 
-let intersectChannel (orien: Orientation) (topLeft: XYPos) (len: float) (wirel: Wire list) =
-
-    let border = topLeft, {topLeft with X = topLeft.X + len}
-    let coordList =
-        List.map (fun w -> (segmentsToIssieVertices w.Segments w)[3..4]) wirel
-        |> List.map ( List.map (fun (x, y, _) -> (x, y) ) )
-        |> List.map ( fun [x;y] -> (x, y) )
-    List.filter (overlap2D border) coordList
-
-    match orien with
-    | Vertical -> 
-        let line =
-            (segmentsToIssieVertices wire.Segments wire)[3..4]
-            |> List.map (fun (x,y,man) -> x,y)
-        let cross = fst line[0] 
-        []
+let getWiresInChannel (channel: BoundingBox) (wires: Wire list): ConnectionId list =
     
-    | Horizontal -> []
+    let listToTup (l: (float * float) list): XYPos * XYPos =
+        {X = fst l[0]; Y = snd l[0]}, {X = fst l[1]; Y = snd l[1]}
+    
+    let wireCoordList =
+        List.map (fun w -> (segmentsToIssieVertices w.Segments w)[3..4], w.WId) wires
+        |> List.map (fun (verts, wid) ->
+            List.map (fun (x, y, _) -> (x, y) ) verts |> listToTup, wid )
+    
+    let bottomRight =
+        { channel.TopLeft with X = channel.TopLeft.X + channel.W; Y = channel.TopLeft.Y + channel.H }
+    
+    List.filter (fun x -> overlap2D (channel.TopLeft, bottomRight) (fst x)) wireCoordList
+    |> List.map snd
+
 
 let smartChannelRoute 
         (channelOrientation: Orientation) 
         (channel: BoundingBox) 
         (model:Model) 
             :Model =
-    let tl = channel.TopLeft
-    let len = 
-        match channelOrientation with
-        | Vertical -> channel.W
-        | Horizontal -> channel.H
+    let tl = {channel.TopLeft with X = channel.TopLeft.X - channel.W}
+    let _channel = {channel with TopLeft = tl}
 
-    // identify all wires going through channel
     let getWires =
-        List.map (intersectChannel Vertical tl len) (List.ofSeq (Seq.cast model.Wires.Values))
+        getWiresInChannel _channel (List.ofSeq (Seq.cast model.Wires.Values))
+        |> printfn "Wires In Channel: %A"
+        
     printfn $"SmartChannel: channel {channelOrientation}:(%.1f{tl.X},%.1f{tl.Y}) W=%.1f{channel.W} H=%.1f{channel.H}"
     model

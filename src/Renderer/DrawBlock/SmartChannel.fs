abb0ba46a9bd87ed28bae6c94b744fadf544f69b
Zsombito <klapper.zsombor@gmail.com>
2023-02-21 18:24:51 +0000

    Basic channel works

--- src/Renderer/DrawBlock/SmartChannel.fs
+++ src/Renderer/DrawBlock/SmartChannel.fs
@@ -25,21 +25,52 @@ let pipePrint x =
     printfn "%A" x
     x
 
-let segmentsInBounds (bounds: BoundingBox) (model:Model) : Option<List<List<SegmentId>>> =
-    let isInBound (state:List<List<SegmentId>>) (key:ConnectionId)(wire : Wire) =
+
+let segmentShifterHelper (model : Model) (segmentId : SegmentId) (shift : float) : Option<Model> =
+    let index, wireId = segmentId
+    let wire = model.Wires[wireId]
+    try
+        let segments =  wire.Segments |> List.updateAt (index - 1) {wire.Segments[index - 1] with Length = wire.Segments[index - 1].Length + shift} |> List.updateAt (index + 1) {wire.Segments[index + 1] with Length = wire.Segments[index + 1].Length - shift}
+        let updatedWireMap = Map.remove wireId model.Wires |> Map.add wireId {wire with Segments = segments}
+        Some {model with Wires = updatedWireMap}
+    with
+    | e -> None
+
+
+let replaceSegments (model : Model) (bounds : BoundingBox) (orientation : Orientation) (wireList : List<List<SegmentId * XYPos>>): Model =
+    match orientation with
+    | Vertical ->   let increment = bounds.W / float(wireList.Length + 1)
+                    let sortedLst = wireList |> List.map (fun x -> List.head x) |> List.sortBy (fun x -> (snd x).Y) 
+                    let folder (mdl : Model, counter: int) (next : SegmentId * XYPos) : Model*int = 
+                        let opt = segmentShifterHelper mdl (fst next) (bounds.TopLeft.X - bounds.W + (increment * float(counter)) - (snd next).X)
+                        match opt with 
+                        | Some md -> md, (counter + 1) |> pipePrint
+                        | None -> mdl, (counter + 1)
+                    let newModel = ((model, 1), sortedLst) ||> List.fold folder
+                    fst newModel
+
+                    
+    | Horizontal -> model
+                        
+    
+    
+                
+
+let segmentsInBounds (bounds: BoundingBox) (model:Model) : Option<List<List<SegmentId * XYPos>>> =
+    let isInBound (state:List<List<SegmentId * XYPos>>) (key:ConnectionId)(wire : Wire) =
         let cornerFolder ((pos : XYPos , pseg : Segment), orientation: Orientation) (seg : Segment) =
             if orientation = Horizontal then (({pos with X = pos.X + seg.Length}, seg), Vertical)
                                       else (({pos with Y = pos.Y + seg.Length}, seg), Horizontal)
         let corners: ((XYPos * Segment) * Orientation) list = 
             (((wire.StartPos, List.head wire.Segments),wire.InitialOrientation),wire.Segments) ||> List.scan cornerFolder
             |> List.tail 
-        let folder (state : List<SegmentId>) ((pos: XYPos, seg:Segment) , orientation:Orientation) =
+        let folder (state : List<SegmentId * XYPos>) ((pos: XYPos, seg:Segment) , orientation:Orientation) =
             match orientation with 
             | Horizontal -> match (bounds.TopLeft.X > pos.X, bounds.TopLeft.X - bounds.W < pos.X, (pos.Y < bounds.TopLeft.Y) <> (pos.Y - seg.Length < bounds.TopLeft.Y)) with
-                            | true,true,true -> state @ [seg.Index, seg.WireId]
+                            | true,true,true -> state @ [(seg.Index, seg.WireId), pos]
                             | _ -> state
             | Vertical -> match (bounds.TopLeft.Y < pos.Y, bounds.TopLeft.Y + bounds.H > pos.Y, (pos.X < bounds.TopLeft.X) <> (pos.X - seg.Length < bounds.TopLeft.X)) with
-                            | true,true,true -> state @ [seg.Index, seg.WireId]
+                            | true,true,true -> state @ [(seg.Index, seg.WireId), pos]
                             | _ -> state
         let lst = ([], corners) ||> List.fold folder 
         match lst.Length with 
@@ -77,5 +108,6 @@ let smartChannelRoute
             :Model =
     let tl = channel.TopLeft
     printfn $"SmartChannel: channel {channelOrientation}:(%.1f{tl.X},%.1f{tl.Y}) W=%.1f{channel.W} H=%.1f{channel.H}"
-    segmentsInBounds channel model |> Option.isSome |> printfn "%A"
-    model
+    match segmentsInBounds channel model with
+        | Some (channelSeg: List<List<SegmentId * XYPos>>) -> channelSeg |> replaceSegments model channel channelOrientation
+        | None -> model

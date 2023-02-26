ef4e629b12b37bf97504e2ce50e0a28998951f3c
Derek Lai <medereklai@gmail.com>
2023-02-26 18:03:59 +0000

    Moved helper functions to SmartHelpers.fs and added Docs

--- src/Renderer/DrawBlock/SmartChannel.fs
+++ src/Renderer/DrawBlock/SmartChannel.fs
@@ -21,57 +21,10 @@ open Operators
     this purpose.
 *)
 
-
-module Constants =
-    let ChannelPolygon = { defaultPolygon with StrokeWidth = "1px"; Fill = "red"; FillOpacity = 0.2 }
-
-///
-/// HLP23: suggested initial smartChannel top-level function
-/// to be tested, it must be given a channel in through which to route wires nicely
-/// Normally the channel will come from symbol edges.
-/// The function must identify all wires with segments going through the channel and space them
-/// This function routes the middle segment of all 7 segment wires by moving them perpendicular to its direction.
-/// It is expected that all wires provided are 7 segment with middle segment in the same direction
-/// wires not so will be ignored.
-/// The messiness of when to call it, what to do with different types of wire, which wires to call it with
-/// could be left till later.
-/// For this simple routing only one dimension of the channel is needed (determined by orientation).
-/// The Wires going through the channel must be returned as an updated Wires map in model.
-// HLP23: need to update XML doc comments when this function is fully worked out.
-
-
-// Retrieves XYPos of every vertex in a wire
-let getWireSegmentsXY (wire: Wire) =
-    
-    let tupToXY (l: (float * float)): XYPos =
-        {X = fst l; Y = snd l}
-
-    segmentsToIssieVertices wire.Segments wire
-    |> List.map (fun (x, y, _) -> (x, y))
-    |> List.map tupToXY    
-
-// Retrieves all wireId's which intersect an arbitrary bounding box
-let getWiresInBox (box: BoundingBox) (model: Model): ConnectionId list =
-    
-    let wires = (List.ofSeq (Seq.cast model.Wires.Values))
-    
-    let is7Seg (wire: Wire): bool =
-        wire.Segments.Length = 7
-        
-    let wireCoordList =
-        List.filter is7Seg wires
-        |> List.map (fun w -> getWireSegmentsXY w, w.WId)
-        |> List.map (fun (posL, wid) -> (posL[3], posL[4]), wid)
-    
-    let bottomRight =
-        { box.TopLeft with X = box.TopLeft.X + box.W; Y = box.TopLeft.Y + box.H }
-    
-    List.filter (fun x -> overlap2D (box.TopLeft, bottomRight) (fst x)) wireCoordList
-    |> List.map snd
-
-
-let extractWire (model: Model) (wId: ConnectionId): Wire = model.Wires[wId]
-
+/// Automatically moves all 7-seg wires which intersect the channel
+/// 1. Detects all 7-seg wires in the model which pass through the channel
+/// 2. Sorts them in the ideal order so that wires are separated in the channel, if possible
+/// 3. Moves wires so that they are equidistant to one another in the channel
 let smartChannelRoute 
         (channelOrientation: Orientation) 
         (channel: BoundingBox) 
@@ -79,10 +32,11 @@ let smartChannelRoute
             : Model =
     
     let tl = channel.TopLeft
-
+    let extractWire (wId: ConnectionId): Wire = model.Wires[wId]
+    // Sorts all wire IDs so that all the wires through the channel will be separated if possible
     let sortWires (wireIds: ConnectionId list) =
         let wire23Pos =
-            List.map (fun wid -> wid, extractWire model wid) wireIds
+            List.map (fun wid -> wid, extractWire wid) wireIds
             |> List.map (fun (wid, w) -> wid, getWireSegmentsXY w)
             |> List.map (fun (wid, l) -> wid, l[2..3])
         
@@ -99,6 +53,7 @@ let smartChannelRoute
                 
         sorted wire23Pos
 
+    // Moves all the wire IDs which are passed in from their old position to the new position in the channel
     let moveWires (model: Model) (sortedWires: (ConnectionId * float) list) =
         let wireChannels =
             match channelOrientation with
@@ -114,14 +69,14 @@ let smartChannelRoute
         // In a 7 segment wire, seg index 2,3,4 are the ones which control the bend
         let adjustWire (model: Model) (newPos: float) ((wid, oldPos): ConnectionId * float) =
             // shorten or lengthen seg 2 & 4
-            let currentWire = extractWire model wid
+            let currentWire = extractWire wid
             let currentSegments = currentWire.Segments
             let newWire =
                 {
-                    extractWire model wid with
+                    extractWire wid with
                         Segments =
                             currentSegments[0..1] @
-                            [{currentSegments[2] with Length = currentSegments[2].Length + (newPos - oldPos)}] @
+                            [{currentSegments[2]  with Length = currentSegments[2].Length + (newPos - oldPos)}] @
                             [currentSegments[3]] @
                             [{currentSegments[4] with Length = currentSegments[4].Length - (newPos - oldPos)}] @ 
                             currentSegments[5..6]
@@ -129,13 +84,10 @@ let smartChannelRoute
             { model with Wires = Map.add wid newWire model.Wires }
             
         (model, wireChannels, sortedWires) |||> List.fold2 adjustWire
-
-    
-    let sortedWires =
-        getWiresInBox channel model
-        |> sortWires
     
     printfn $"SmartChannel: channel {channelOrientation}:(%.1f{tl.X},%.1f{tl.Y}) W=%.1f{channel.W} H=%.1f{channel.H}"
     
-    moveWires model sortedWires
+    getNSegmentWiresInBox 7 channel model
+    |> sortWires
+    |> moveWires model
     

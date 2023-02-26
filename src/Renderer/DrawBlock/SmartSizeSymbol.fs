b5a16b9e17dd899af68b868f03a9b6f113bd0f5e
jeanpi101 <bartosz.rzepala20@imperial.ac.uk>
2023-02-26 21:51:40 +0000

    Added basic resizing

--- src/Renderer/DrawBlock/SmartSizeSymbol.fs
+++ src/Renderer/DrawBlock/SmartSizeSymbol.fs
@@ -28,6 +28,26 @@ open Operators
 /// It will do nothing if symbolToOrder is not a Custom component (which has adjustable size).
 /// HLP23: when this function is written replace teh XML comment by something suitable concisely
 /// stating what it does.
+/// 
+let Scale 
+    (symbolScale: float option)
+        : float =
+    match symbolScale with
+    | Some x -> x
+    | None -> 1.0
+
+let getPortDist 
+    (symbol: Symbol) 
+    (pos: Edge) 
+        : float = 
+    let Width = symbol.Component.W * Scale symbol.HScale
+    printfn $"Width of symbol {Width}"
+    let NoPorts = List.length symbol.PortMaps.Order[pos]
+    Width / ((float NoPorts) + 1.0)
+
+
+
+
 let reSizeSymbol 
     (wModel: BusWireT.Model) 
     (symbolToSize: Symbol) 
@@ -35,10 +55,23 @@ let reSizeSymbol
         : BusWireT.Model =
     printfn $"ReSizeSymbol: ToResize:{symbolToSize.Component.Label}, Other:{otherSymbol.Component.Label}"
     let sModel = wModel.Symbol
+    //printfn "%A" wModel.Wires
 
+    let WidthToChange = getPortDist symbolToSize Top
+    printfn "%A" WidthToChange
+
+    let WidthOfConstant = getPortDist otherSymbol Bottom
+    printfn "%A" WidthOfConstant
+    let ScaleFactor = WidthOfConstant/WidthToChange
+    printfn "%A" ScaleFactor
     let wires = [] // replace this with correct wires
+    
+    
+    let symbol' = {symbolToSize with HScale = Some (ScaleFactor * Scale symbolToSize.HScale)}
+     // no change at the moment
+    let leng = List.length symbol'.PortMaps.Order[Top]
+    
 
-    let symbol' = {symbolToSize with HScale = Some 0.5} // no change at the moment
     // HLP23: this could be cleaned up using Optics - see SmartHelpers for examples
     {wModel with 
         Wires = wModel.Wires // no change for now, but probably this function should use update wires after resizing.

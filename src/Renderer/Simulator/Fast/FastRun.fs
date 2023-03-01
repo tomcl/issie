e241226d7e322fb20e7eb27636eb366709dffdbe
Tom Clarke <tomcl@users.noreply.github.com>
2023-03-01 17:40:11 +0000

    Remove simStep modulo reduction for comb fastReduce call - not needed?

--- src/Renderer/Simulator/Fast/FastRun.fs
+++ src/Renderer/Simulator/Fast/FastRun.fs
@@ -325,7 +325,7 @@ let private setSimulationInput (cid: ComponentId) (fd: FData) (step: int) (fs: F
 /// input has changed
 let private runCombinationalLogic (step: int) (fastSim: FastSimulation) =
     fastSim.FOrderedComps
-    |> Array.iter (fastReduce fastSim.MaxArraySize (step % fastSim.MaxArraySize) false)
+    |> Array.iter (fastReduce fastSim.MaxArraySize step false)
 
 /// Change an input and make simulation correct. N.B. step must be the latest
 /// time-step since future steps are not rerun (TODO: perhaps they should be!)

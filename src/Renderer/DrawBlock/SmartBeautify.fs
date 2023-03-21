229c2517f54020ea7a843f4933b20c295bd6aad4
bryantan <bet20@ic.ac.uk>
2023-03-21 21:18:51 +0000

    first draft of full beautify

--- src/Renderer/DrawBlock/SmartBeautify.fs
+++ src/Renderer/DrawBlock/SmartBeautify.fs
@@ -10,9 +10,13 @@ open Optic
 open Operators
 open SmartHelpers
 open SmartPortOrder
+open SmartSizeSymbol
 
 module Constants =
     let maxPairsToBeautify = 10
+    let maxOffset = 0
+    let maxScale = { x = 0.2; y = 0.2 }
+    
 
 /// Gets Symbol Pairs to Beautify.
 let getSymsToReorder (model: BusWireT.Model) =
@@ -24,10 +28,32 @@ let getSymsToReorder (model: BusWireT.Model) =
         | And, Custom _ -> true
         | _ -> false
 
-    getConnSyms model |> List.filter keepSymPair
+    getConnSyms model |> List.filter keepSymPair    
+
+let updateBoundingBoxes (wModel: BusWireT.Model) (boundingBoxes: Map<ComponentId, BoundingBox>) = 
+    boundingBoxes |> Map.map (fun sId _ -> getBoundingBox wModel.Symbol sId)
+  
+/// Extracts Symbols to Resize.
+let symsToResize (model: BusWireT.Model) = 
+    model.Symbol.Symbols
+    |> Map.filter (fun id sym ->         
+        match sym.Component.Type with
+        | Custom _ as customComp -> true
+        | _ -> false)
+    |> Map.toList
+    |> List.map snd
+
+let beautifyComp (helpers: ExternalSmartHelpers) (boundingBoxes: Map<CommonTypes.ComponentId, BoundingBox>) (model: BusWireT.Model) = 
+    ((model, boundingBoxes), symsToResize model)
+    ||> List.fold (fun (model', bb') sym -> 
+        let sym' = model'.Symbol.Symbols[sym.Id]
+        let model' = optimiseSymbol model' sym' bb' helpers
+        (model', updateBoundingBoxes model' bb')
+    )
+    |> fst
 
 /// Beautifies Symbol Pairs.
-let beautifyPairs (model: BusWireT.Model) (smartHelpers: ExternalSmartHelpers) =
+let beautifyPairs (smartHelpers: ExternalSmartHelpers) (model: BusWireT.Model) =
 
     let pairs =
         getSymsToReorder model
@@ -41,8 +67,10 @@ let beautifyPairs (model: BusWireT.Model) (smartHelpers: ExternalSmartHelpers) =
         reOrderPorts model' symA' symB' smartHelpers)
 
 /// Header Function.
-let smartBeautify (wModel: BusWireT.Model) (smartHelpers: ExternalSmartHelpers) : BusWireT.Model =
+let smartBeautify (wModel: BusWireT.Model) (boundingBoxes: Map<CommonTypes.ComponentId, BoundingBox>) (smartHelpers: ExternalSmartHelpers) : BusWireT.Model =
 
     printfn "Invoked SmartBeautify Function!"
 
-    beautifyPairs wModel smartHelpers
+    wModel
+    |> beautifyComp smartHelpers boundingBoxes 
+    |> beautifyPairs smartHelpers

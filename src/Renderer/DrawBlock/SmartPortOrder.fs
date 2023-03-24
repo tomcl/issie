752fc11c91886dfb2d65c71a6f6b646b79ac631d
itmr1 <itimur189@gmail.com>
2023-03-24 19:43:02 +0000

    Fixing Rhea Sort

--- src/Renderer/DrawBlock/SmartPortOrder.fs
+++ src/Renderer/DrawBlock/SmartPortOrder.fs
@@ -290,15 +290,15 @@ let sortMultPorts
     (combinedOtherSymbolsOrientation: Map<string,Edge>)
     (combinedOtherSymbolsOrder: Map<Edge,string list>)
        : list<list<string * string * Edge * Edge>> =
-
-       connectedPortsNeeded 
-                |> List.filter (fun (_,lst) -> combinedOtherSymbolsOrientation 
-                                                |> Map.containsKey (lst[0]|> (fun (x,_,_,_)->x)))
-                |> List.map (fun (_,lst)->lst)
-                |> List.map (fun y -> sortByOther(combinedOtherSymbolsOrder 
-                                                    |> Map.find (combinedOtherSymbolsOrientation
-                                                                    |>Map.find (y[0] |> (fun (x,_,_,_)-> x)))) 
-                                                                        y)
+    connectedPortsNeeded 
+        |> List.map (fun (e,lst) ->
+                            List.filter (fun (x,y,z,i)-> combinedOtherSymbolsOrientation |> Map.containsKey x) lst)
+        |> List.filter (fun x -> x<>[])
+        |> List.map (fun y -> 
+                                            sortByOther  (combinedOtherSymbolsOrder 
+                                                                    |> Map.find (combinedOtherSymbolsOrientation
+                                                                                      |>Map.find (y[0] |> (fun (x,_,_,_)-> x)))) 
+                                                         y)
     
 
 // Will only reorder the component with the most wires connected to it.

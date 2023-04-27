916e98d75209f77eb4709bb729e4fbff6580ecf3
Yujie Wang <yujie@wang.icu>
2023-04-27 01:44:39 +0100

    - fix blank waveform in GUI - replace StepArray<FData> with IOArray in Driver-DriverData - eep1x still cause stack overflow

--- src/Renderer/UI/ModelType.fs
+++ src/Renderer/UI/ModelType.fs
@@ -191,7 +191,7 @@ type Wave = {
     /// width of the waveform's bus
     Width: int
     /// Array indexed by clock cycle to show value of wave.
-    WaveValues: StepArray<FData>
+    WaveValues: IOArray
     /// SVG of waveform
     SVG: ReactElement option
 }

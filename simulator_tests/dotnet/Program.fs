cf8de60f1df2281f51530665dfe18bd236a351b2
Yujie Wang <yujie@wang.icu>
2023-05-22 14:48:07 +0100

    update simulator benchmark

--- simulator_tests/dotnet/Program.fs
+++ simulator_tests/dotnet/Program.fs
@@ -1,5 +1,9 @@
 ﻿open FilesIO
 open Helpers.JsonHelpers
+open System.Diagnostics
+open TimeHelpers
+
+let timer = new Stopwatch()
 
 let loadAllComponentFiles (folderPath: string) =
     let x =
@@ -27,25 +31,45 @@ let loadAllComponentFiles (folderPath: string) =
                 filePath |> tryLoadComponentFromPath)
         |> Helpers.tryFindError
 
-let simulationArraySize = 500
-let maxSteps = 2000
 let path = "/home/yujie/workspace/EEE1labs/DECA/Part3-Section1-Lab3/lab3i"
+let simulationArraySize = 500
+let steps = 2000
+let warmup = 5
+let simulationRound = 10
+let benchmarkRound = 20
 
-let runSimulation path =
+let geometricMean (values: float list) =
+    (values |> List.reduce (*)) ** (1.0 / (float values.Length))
+
+let benchmark path =
     let loadedComps =
         match loadAllComponentFiles path with
         | Error msg -> failwithf "Error: %s" msg
         | Ok loadedComps -> loadedComps
 
     loadedComps
-    |> List.map (fun topComp ->
-        printfn "Running Simulation for component %A" topComp.Name
+    |> List.map (fun c ->
+        printfn "Running Simulation for component %A" c.Name
 
-        match Simulator.startCircuitSimulation simulationArraySize topComp.Name topComp.CanvasState loadedComps with
+        match Simulator.startCircuitSimulation simulationArraySize c.Name c.CanvasState loadedComps with
         | Error e -> failwithf "%A" e
         | Ok simData ->
             let fastSim = simData.FastSim
-            FastRun.runFastSimulation None 2000 fastSim |> ignore
-            printfn "%A" fastSim.ClockTick)
+            let comps = fastSim.FOrderedComps |> Array.length
 
-runSimulation path
+            [ 1 .. (warmup + simulationRound) ]
+            |> List.map (fun x ->
+                fastSim.ClockTick <- 0
+                let start = TimeHelpers.getTimeMs ()
+                FastRun.runFastSimulation None steps fastSim |> ignore
+                TimeHelpers.getTimeMs () - start)
+            |> List.skip warmup
+            |> List.map (fun time -> float (steps * comps) / time)
+            |> List.average)
+    |> geometricMean
+
+[ 1..benchmarkRound ]
+|> List.map (fun i -> 
+    printfn "========== %A ==========" i
+    benchmark path)
+|> printfn "%A"

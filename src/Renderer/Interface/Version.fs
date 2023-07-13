89ffb5e03650282f67d3eb86f7af81c03712482b
Tom Clarke <tomcl@users.noreply.github.com>
2023-07-13 18:10:38 +0100

    change build script names; make VERSION more flexible
    1)
    dev -> debug
    devfast -> dev
    
    2)
    allow strings in version string so it can be 4.0.a.1

--- src/Renderer/Interface/Version.fs
+++ src/Renderer/Interface/Version.fs
@@ -1,13 +1,13 @@
 ﻿module Version
-let VERSION = [ 4 ; 0 ; 1 ]
+let VERSION = [ "4" ; "0" ; "a"; "1" ]
 
 // The first 12  white-space separated words in this file must be in the above format - note that spaces are required.
 // This works as valid F# data for displaying the code version and can also be read programmatically from the master branch github file
 
 let VersionString = 
-    if List.length VERSION <> 3 then failwithf "Badly formatted version %A (VERSION must be list of 3 integers)" VERSION
+    if List.length VERSION < 3 then failwithf "Badly formatted version %A (VERSION must be list of 3 or more strings)" VERSION
     VERSION 
-    |> List.map (fun (i:int)-> i.ToString()) |> String.concat "."
+    |> List.map (fun (i:string)-> i.ToString()) |> String.concat "."
     |> fun s -> "v" + s
 
 

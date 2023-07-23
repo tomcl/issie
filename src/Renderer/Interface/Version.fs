b5fad22bee79c5607de0e047a8cfcb4fc666b851
Tom Clarke <tomcl@users.noreply.github.com>
2023-07-23 15:38:38 +0100

    Bump to 4.0.a.4
    Bump to 4.0.a.4
    
    Tidy up some one-off testing code

--- src/Renderer/Interface/Version.fs
+++ src/Renderer/Interface/Version.fs
@@ -1,5 +1,5 @@
 ﻿module Version
-let VERSION = [ "4" ; "0" ; "a";  "3" ]
+let VERSION = [ "4" ; "0" ; "a";  "4" ]
 
 // The first 12  white-space separated words in this file must be in the above format - note that spaces are required.
 // This works as valid F# data for displaying the code version and can also be read programmatically from the master branch github file
@@ -10,49 +10,7 @@ let VersionString =
     |> List.map (fun (i:string)-> i.ToString()) |> String.concat "."
     |> fun s -> "v" + s
 
-open CommonTypes
 
-module TestDU =
-
-    type ComponentType =
-        | Input of BusWidth: int * DefaultValue: int option
-        | Input1 of BusWidth: int
-        | Output of BusWidth: int
-        | Viewer of BusWidth: int
-        | IOLabel
-        | NotConnected
-
-    type Component = {
-            Id : string
-            Type : ComponentType // This is JSONComponent.ComponentType!
-        }
-
-    let makeDummyComponents() =
-        [1..2]
-        |> List.map (fun n -> {Id= $"{n}"; Type = NotConnected})
-
-    let testDUEquality() =
-        let testCases = [NotConnected; Input(1,Some 1); Input(0,Some 0)]
-        List.allPairs testCases testCases
-        |> List.iter (fun (a,b) -> printfn $"a={a}, b={b}, a=b={a=b}")
-
-
-        let comps = makeDummyComponents()
-        printfn $"\nDummy components are {comps}\n"
-        
-
-        let checkCompsForId (id:string) =
-            comps
-            |> List.tryFind (fun comp ->
-                comp.Id = id && comp.Type = NotConnected)
-            |> function
-                | Some _ -> true
-                | None -> false
-            |> fun res -> printfn $"checking: id={id} ->  Found = {res}"
-
-        checkCompsForId "1"
-        checkCompsForId "2"
-        checkCompsForId "3"
 
 
 

0bd4ebbbbec233de5063fdfef682204b1133e0cf
Shiyizhuanshi <hj1021@ic.ac.uk>
2024-02-23 12:15:35 +0000

    complete T2R

--- src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
+++ src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
@@ -7,6 +7,52 @@ open SheetT
 open Symbol
 open BlockHelpers
 
+
+
+//-----------------------------------------------------------------------------------------------
+// visibleSegments is included here as ahelper for info, and because it is needed in project work
+//-----------------------------------------------------------------------------------------------
+
+/// The visible segments of a wire, as a list of vectors, from source end to target end.
+/// Note that in a wire with n segments a zero length (invisible) segment at any index [1..n-2] is allowed 
+/// which if present causes the two segments on either side of it to coalesce into a single visible segment.
+/// A wire can have any number of visible segments - even 1.
+let visibleSegments (wId: ConnectionId) (model: SheetT.Model): XYPos list =
+
+    let wire = model.Wire.Wires[wId] // get wire from model
+
+    /// helper to match even and off integers in patterns (active pattern)
+    let (|IsEven|IsOdd|) (n: int) = match n % 2 with | 0 -> IsEven | _ -> IsOdd
+
+    /// Convert seg into its XY Vector (from start to end of segment).
+    /// index must be the index of seg in its containing wire.
+    let getSegmentVector (index:int) (seg: BusWireT.Segment) =
+        // The implicit horizontal or vertical direction  of a segment is determined by 
+        // its index in the list of wire segments and the wire initial direction
+        match index, wire.InitialOrientation with
+        | IsEven, BusWireT.Vertical | IsOdd, BusWireT.Horizontal -> {X=0.; Y=seg.Length}
+        | IsEven, BusWireT.Horizontal | IsOdd, BusWireT.Vertical -> {X=seg.Length; Y=0.}
+
+    /// Return a list of segment vectors with 3 vectors coalesced into one visible equivalent
+    /// if this is possible, otherwise return segVecs unchanged.
+    /// Index must be in range 1..segVecs
+    let tryCoalesceAboutIndex (segVecs: XYPos list) (index: int)  =
+        if segVecs[index] =~ XYPos.zero
+        then
+            segVecs[0..index-2] @
+            [segVecs[index-1] + segVecs[index+1]] @
+            segVecs[index+2..segVecs.Length - 1]
+        else
+            segVecs
+
+    wire.Segments
+    |> List.mapi getSegmentVector
+    |> (fun segVecs ->
+            (segVecs,[1..segVecs.Length-2])
+            ||> List.fold tryCoalesceAboutIndex)
+
+
+
 /// key: B1R Type: R Descrip: The dimensions of a custom component symbol
 let getCustomSymbolDimensions (sym: SymbolT.Symbol) :  float * float =
     let height = sym.Component.H
@@ -88,15 +134,39 @@ let getIntersectingSymbolsCount (sheetModel:SheetT.Model) : int =
 /// key: T2R Type: R Descrip: The number of distinct wire visible segments that intersect with one or more symbols.
 /// See Tick3.HLPTick3.visibleSegments for a helper. Count over all visible wire segments.
 let getIntersectingWireSegmentsCount (sheetModel:SheetT.Model) : int =
-    failwith "Not Implemented"
+    let segments = 
+        sheetModel.Wire.Wires
+        |> Helpers.mapValues 
+        |> Array.toList
+        |> List.collect (fun wire -> getNonZeroAbsSegments wire)
+    
+    let symbolBoundingBoxes =
+        sheetModel.Wire.Symbol.Symbols
+        |> Helpers.mapValues
+        |> Array.toList
+        |> List.map getSymbolBoundingBox
+
+    //return true if seg intersects with any symbol
+    let segIntersectWithSymbol (seg: BusWireT.ASegment) : bool =
+        let intersectBoxCount =
+            symbolBoundingBoxes
+            |> List.map (fun box -> segmentIntersectsBoundingBox box seg.Start seg.End )
+            |> List.filter (fun b -> b.IsSome)
+            |> List.length
+        match intersectBoxCount with
+        | 0 -> false
+        | _ -> true
+        
+    segments
+    |> List.filter (fun seg -> segIntersectWithSymbol seg)
+    |> List.length
+
+
 
 /// key: T3R Type: R Descrip: The number of distinct pairs of segments that cross each other at right angles. 
 /// Does not include 0 length segments or segments on same net intersecting at one end, 
 /// or segments on same net on top of each other. Count over whole sheet.
-let getRightAngleCrossingSegmentsCount (sheetModel:SheetT.Model) : int =
-    // BusWire.getASegmentFromId
-    // BusWire.getSegmentOrientation
-    
+let getRightAngleCrossingSegmentsCount (sheetModel:SheetT.Model) : int =    
     failwith "Not Implemented"
 
 /// key: T4R Type: R Sum of wiring segment length, counting only one when there are N same-net 
@@ -107,7 +177,35 @@ let getVisibleWireLength (sheetModel:SheetT.Model) : float =
 
 /// key: T5R Type: R Descrip: Number of visible wire right-angles. Count over whole sheet.
 let getVisibleWireRightAnglesCount (sheetModel:SheetT.Model) : int =
-    failwith "Not Implemented"
+    let segmentPairsList (wire: BusWireT.Wire) = 
+        let segmentsList=
+            wire.Segments
+            |> List.mapi (fun i seg -> i, seg.WireId)
+        List.map2 (fun segmentId1 segmentId2 -> segmentId1, segmentId2) segmentsList (List.tail segmentsList)
+    
+    let segmentStartAndEndPos (segmentId : SegmentId): (XYPos * XYPos) = 
+        let ASegment = BusWire.getASegmentFromId sheetModel.Wire segmentId
+        ASegment.Start, ASegment.End
+
+    let segmentsRightAngle (seg1: SegmentId) (seg2: SegmentId) = 
+        let seg1Orientation = BusWire.getSegmentOrientation <|| (segmentStartAndEndPos seg1)
+        let seg2Orientation = BusWire.getSegmentOrientation <|| (segmentStartAndEndPos seg2)
+        if seg1Orientation = seg2Orientation then false
+        else true
+
+    let countRightAnglePairs (wire: BusWireT.Wire) =
+        segmentPairsList wire
+        |> List.filter (fun (segmentId1 , segmentId2) -> segmentsRightAngle segmentId1 segmentId2)
+        |> List.length
+
+    sheetModel.Wire.Wires
+    |> Helpers.mapValues
+    |> Array.toList
+    |> List.map countRightAnglePairs
+    |> List.sum
+
+
+
 
 /// key: T6R Type: R Descrip: The zero-length segments in a wire with non-zero segments on either side that have 
 /// Lengths of opposite signs lead to a wire retracing itself. Note that this can also apply 

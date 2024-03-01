7ac52303bd3d41b411b10d1fca8551a5d0c0ee29
edzchen747 <edzchen747@gmail.com>
2024-03-01 00:40:01 +0000

    D2 individual implementation work

---
+++ src/Renderer/DrawBlock/SheetBeautifyD2.fs
@@ -0,0 +1,77 @@
+﻿module SheetBeautifyD2
+// open modules likely to be used
+open CommonTypes
+open DrawHelpers
+open DrawModelType
+open DrawModelType.SymbolT
+open DrawModelType.BusWireT
+open DrawModelType.SheetT
+open SheetUpdateHelpers
+open SheetBeautifyHelpers
+open Optics
+open Operators
+
+let sheetOrderFlip (model: SheetT.Model) : SheetT.Model =
+    let symbols =
+        model.Wire.Symbol.Symbols
+        |> Map.toList
+
+    let filterCustomComponents (symbols: (ComponentId * Symbol) list) =
+        symbols |> List.filter (fun (id,symbol) -> match symbol.Component.Type with
+                                                    | Custom _ -> true
+                                                    | _ -> false)
+
+    let rec swapPortsOnEdge ((model,id,symbol): SheetT.Model * ComponentId * Symbol) (edge: Edge) =
+
+        let generateSwaps (order: string list) : (string list list) =
+            let possibleOrders = order.Length
+
+            let swapElementsAtIndexes index1 index2 list =
+                list
+                |> List.mapi (fun i x ->
+                    if i = index1 then
+                        List.item index2 list
+                    elif i = index2 then
+                        List.item index1 list
+                    else
+                        x
+                )
+
+            order
+            |> List.replicate possibleOrders
+            |> List.mapi (fun i order ->
+                if i < possibleOrders-1 then
+                    swapElementsAtIndexes i (i+1) order
+                else
+                    swapElementsAtIndexes i 0 order
+            )
+
+        let checkSwapFolder ((improvement,model): bool * SheetT.Model) (order: string list)  =
+            let beforeCount = countCrossingWires model
+            let model' = Optic.set symbols_ (Map.add id (setPortOrder model.Wire.Symbol.Symbols[id] edge order) model.Wire.Symbol.Symbols) model
+            let afterCount = countCrossingWires model'
+
+            if afterCount < beforeCount
+            then true,model'
+            else false,model
+                
+        let swappedPortsList =
+            symbol.PortMaps.Order[edge]
+            |> generateSwaps
+
+        let improvement, bestModel = ((false,model), swappedPortsList) ||> List.fold checkSwapFolder 
+
+        if improvement then swapPortsOnEdge (bestModel,id,symbol) edge
+        else (bestModel,id,symbol)
+
+    let swapSymbolEdgePorts (model: SheetT.Model) ((id,symbol): ComponentId * Symbol) =
+        let edgesWithPorts = 
+            [Top;Bottom;Left;Right]
+            |> List.filter (fun edge -> Map.containsKey edge symbol.PortMaps.Order)
+        let model',_,_ = ((model,id,symbol), edgesWithPorts) ||> List.fold swapPortsOnEdge
+        model'
+
+    
+    let customComponentSymbols = filterCustomComponents symbols
+    let (model') = (model,customComponentSymbols) ||> List.fold swapSymbolEdgePorts 
+    model'
--- src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
+++ src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
@@ -54,7 +54,7 @@ let visibleSegments (model: SheetT.Model) (wId: ConnectionId): XYPos list =
 
 
 // B1R, B1W
-// The dimensions of a custom component symbol 
+/// The dimensions of a custom component symbol 
 let getCustDimensions (symbol: Symbol) : float * float =
     let width = Optic.get w_ symbol.Component
     let height = Optic.get h_ symbol.Component
@@ -68,12 +68,12 @@ let setCustDimensions (symbol: Symbol) ((width,height) : float*float) : Symbol =
     {symbol with Component = newComponent}
 
 // B2W
-// The position of a symbol on the sheet 
+/// The position of a symbol on the sheet 
 let setPosition (symbol: Symbol) (newPos: XYPos) : Symbol =
     Optic.set posOfSym_ newPos symbol
 
 // B3R, B3W
-// Read/write the order of ports on a specified side of a symbol 
+/// Read/write the order of ports on a specified side of a symbol 
 let getPortOrder (symbol: Symbol) (side: Edge) : string list option =
     Map.tryFind side symbol.PortMaps.Order
 
@@ -86,7 +86,7 @@ let setPortOrder (symbol: Symbol) (side: Edge) (newOrder: string list) : Symbol 
     | None -> failwithf $"No ports on symbol side: side='{side}', Symbol='{symbol}"
 
 // B4R, B4W
-// The reverses state of the inputs of a MUX2 
+/// The reverses state of the inputs of a MUX2 
 let getMux2Reverse (symbol: Symbol) =
     symbol.ReversedInputPorts
 
@@ -94,18 +94,18 @@ let setMux2Reverse (symbol: Symbol) (reverse : bool option) =
     {symbol with ReversedInputPorts = reverse}
 
 // B5R
-// The position of a port on the sheet. It cannot directly be written. 
+/// The position of a port on the sheet. It cannot directly be written. 
 let getPortPos (symbol: Symbol) (port: Port): XYPos =
     let portOffset = Symbol.getPortPos symbol port
     portOffset + symbol.Pos
 
 // B6R
-// The Bounding box of a symbol outline (position is contained in this) 
+/// The Bounding box of a symbol outline (position is contained in this) 
 let getBoundBox (symbol: Symbol) : BoundingBox =
     symbol.LabelBoundingBox
 
 // B7R, B7W
-// The rotation state of a symbol 
+/// The rotation state of a symbol 
 let getSymRotation (symbol: Symbol) : Rotation =
     symbol.STransform.Rotation
 
@@ -113,7 +113,7 @@ let setSymRotation (symbol: Symbol) (rotation: Rotation) : Symbol =
     {symbol with STransform = {symbol.STransform with Rotation = rotation}}
 
 // B8R, B8W
-// The flip state of a symbol 
+/// The flip state of a symbol 
 let getSymFlip (symbol: Symbol) : bool =
     symbol.STransform.Flipped
 
@@ -122,7 +122,7 @@ let setSymFlip (symbol: Symbol) (flip: bool) : Symbol =
 
 
 // T1R
-// The number of pairs of symbols that intersect each other. See Tick3 for a related function. Count over all pairs of symbols. 
+/// The number of pairs of symbols that intersect each other. See Tick3 for a related function. Count over all pairs of symbols. 
 let countSymbolIntersectSymbols (sheet: SheetT.Model) : int =
             let boxes =
                 Helpers.mapValues sheet.BoundingBoxes
@@ -133,8 +133,8 @@ let countSymbolIntersectSymbols (sheet: SheetT.Model) : int =
             |> List.length
 
 // T2R
-// The number of distinct wire visible segments that intersect with one or more symbols. See Tick3.HLPTick3.visibleSegments for a helper.
-// Count over all visible wire segments. 
+/// The number of distinct wire visible segments that intersect with one or more symbols. See Tick3.HLPTick3.visibleSegments for a helper.
+/// Count over all visible wire segments. 
 let countWireIntersectSymbols (model: SheetT.Model) : int =
     let boxes =
         Helpers.mapValues model.BoundingBoxes
@@ -153,8 +153,8 @@ let countWireIntersectSymbols (model: SheetT.Model) : int =
     |> List.length
     
 // T3R
-// The number of distinct pairs of segments that cross each other at right angles. Does not include 0 length segments or segments on same
-// net intersecting at one end, or segments on same net on top of each other. Count over whole sheet. 
+/// The number of distinct pairs of segments that cross each other at right angles. Does not include 0 length segments or segments on same
+/// net intersecting at one end, or segments on same net on top of each other. Count over whole sheet. 
 let countCrossingWires (model:SheetT.Model) : int =    
     let segList = 
         model.Wire.Wires
@@ -174,8 +174,8 @@ let countCrossingWires (model:SheetT.Model) : int =
 
 
 // T4R
-// Sum of wiring segment length, counting only one when there are N same-net segments overlapping (this is the visible wire length on the sheet).
-// Count over whole sheet. 
+/// Sum of wiring segment length, counting only one when there are N same-net segments overlapping (this is the visible wire length on the sheet).
+/// Count over whole sheet. 
 let visibleWireLength (model: SheetT.Model) : float =
 
     let hasOverlap (seg1: ASegment) (seg2: ASegment) =
@@ -190,13 +190,13 @@ let visibleWireLength (model: SheetT.Model) : float =
         |> List.collect getNonZeroAbsSegments
         |> List.partition (fun seg -> seg.Start.X = seg.End.X)
 
-    // split into a two lists based on whether a segment overlaps another in the list
+    /// split into a two lists based on whether a segment overlaps another in the list
     let splitByOverlaps segList =
         let comparingList = segList
         segList
         |> List.partition (fun seg -> List.exists (hasOverlap seg) comparingList)
 
-    // finds the total length of a list of overlap wires
+    /// finds the total length of a list of overlap wires
     let getOverlapLength (segList: ASegment list)=
         let minStartSeg =
             segList
@@ -207,14 +207,14 @@ let visibleWireLength (model: SheetT.Model) : float =
         let lengthXY =  maxEndSeg.End - minStartSeg.Start
         abs(lengthXY.X + lengthXY.Y)
 
-    // groups horizontal overlaps by their starting point and calculates overall length
+    /// groups horizontal overlaps by their starting point and calculates overall length
     let HorizontalOverlapsLength (segList: ASegment list)=
         segList
         |> List.groupBy (fun seg -> seg.Start.X)
         |> List.collect snd
         |> getOverlapLength
 
-    // groups vertical overlaps by their starting point and calculates overall length
+    /// groups vertical overlaps by their starting point and calculates overall length
     let VerticalOverlapsLength (segList: ASegment list)=
         segList
         |> List.groupBy (fun seg -> seg.Start.X)
@@ -240,27 +240,27 @@ let visibleWireLength (model: SheetT.Model) : float =
     
 
 // T5R
-// Number of visible wire right-angles. Count over whole sheet.
+/// Number of visible wire right-angles. Count over whole sheet.
 let countWireRightAngles (model: SheetT.Model) : int =
     model.Wire.Wires
     |> Map.toList
     |> List.map fst
     |> List.collect (fun wId -> model.Wire.Wires[wId].Segments)
     |> List.fold (fun acc seg -> if seg.Length = 0 then acc - 2 else acc + 1) -1
-
+        
 // T6R
-// The zero-length segments in a wire with non-zero segments on either side that have Lengths of opposite signs lead to a wire retracing itself.
-// Note that this can also apply at the end of a wire (where the zero-length segment is one from the end). This is a wiring artifact that should
-// never happen but errors in routing or separation can cause it. Count over the whole sheet. Return from one function a list of all the segments
-// that retrace, and also a list of all the end of wire segments that retrace so far that the next segment (index = 3 or Segments.Length – 4) -
-// starts inside a symbol.
-// Returns 
+/// The zero-length segments in a wire with non-zero segments on either side that have Lengths of opposite signs lead to a wire retracing itself.
+/// Note that this can also apply at the end of a wire (where the zero-length segment is one from the end). This is a wiring artifact that should
+/// never happen but errors in routing or separation can cause it. Count over the whole sheet. Return from one function a list of all the segments
+/// that retrace, and also a list of all the end of wire segments that retrace so far that the next segment (index = 3 or Segments.Length – 4) -
+/// starts inside a symbol.
+/// Returns 
 let retracingSegments (model: SheetT.Model) : (Segment list * Segment list) =
 
     let get3ConsecutiveSegments (segList: Segment list) = 
         List.windowed 3 segList
 
-    // checks for retrace at the beginning and end of segment list
+    /// checks for retrace at the beginning and end of segment list
     let hasRetraceInSymbol (comb: Segment list) =
         if (comb[0].Length = 0 && (comb[1].Length *  comb[2].Length) < 0)// eg. [0;-5;10]
         then [comb[0]]
@@ -268,7 +268,7 @@ let retracingSegments (model: SheetT.Model) : (Segment list * Segment list) =
         then [comb[2]]
         else []
 
-    // checks for retrace in the middle of segment list
+    /// checks for retrace in the middle of segment list
     let hasRetrace (comb: Segment list) =
         if (comb[1].Length = 0 && (comb[0].Length * comb[2].Length) < 0) // eg. [5;0;-10], [-5;0;10]
         then [comb[1]]

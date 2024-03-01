e2a9d933d7220441e2a496580f149a8faea2f5a9
yuxuan-chen-6 <cyx20130602@gmail.com>
2024-03-01 13:52:59 +0000

    written function to align two demension(space between two ports)

--- README-TEAM-yc3521.md
+++ README-TEAM-yc3521.md
@@ -1,10 +1,15 @@
 ## Team-phase work by yc3521
-D1 build: in SheetBeautifyD1.fs (module SheetBeautifyD1)
+D1 build: in SheetBeautifyD1.fs.
+Objective: Reducing total wire segments without increasing wire crossings by scaling the Symbols and translation on the sheet.
 
-Contribution to team-phase work: implemented some fundamental functions required for D1.
+
+### Summary of how objective can be achieved:
+1. We need to find the arrangement such that the number of input and output ports that are either horizontally or vertically on the same line is maximized, because this can make sure the number of straight lines connecting inputs and outputs are max.
+
+2. To achieve this arrangement, we need to determine how many input-output ports are on opposite sides of two symbols. Then find out the opposite sides that has the most input-output wire that are either on the same horizontal or vertical line by scaling the symbols(for more than two input-output paris, we can at least align two pairs by scaling and translation).
+
+3. Use the concept of two symbols arrangemengt, search all pairs of symbols on the sheet and rank the pairs with highest being the pair of symbols with the side that has the most same line input-output ports.
 
 ### Implemented functionalities useful in D3 Build:
 
 
-
-### Remaining work in D3 to be done:
--- src/Renderer/DrawBlock/SheetBeautifyD1.fs
+++ src/Renderer/DrawBlock/SheetBeautifyD1.fs
@@ -33,7 +33,7 @@ let isOppositeSide (sym1: Symbol) (p1:Port) (sym2: Symbol) (p2:Port) =
     | (Some side1, Some side2) -> side1.Opposite = side2
     | _ -> false
 
-// generate two lists of all the ports that are connected to a given port(one of them is empty)
+// generate lists of all the ports that are connected to a given port(one of them is empty so only output one list)
 let connectedPorts (model: SheetT.Model) (port: Port) =
     let inputPorts, outputPorts = 
         model.Wire.Wires 
@@ -45,4 +45,78 @@ let connectedPorts (model: SheetT.Model) (port: Port) =
             | PortType.Output, inputPort, OutputPortId id when id = port.Id -> (inputPort :: accInput, accOutput)
             | _ -> (accInput, accOutput)
         ) ([], [])
-    (inputPorts, outputPorts)
+    // return the list of connected ports as string list
+    match port.PortType with
+    | PortType.Input -> outputPorts |> List.map (fun (OutputPortId id) -> id)
+    | PortType.Output -> inputPorts |> List.map (fun (InputPortId id) -> id)
+
+// Align the scale of the symbols such that two ports are on the same axis, make sure it is opposite side already
+let alignScale (model: SymbolT.Model) 
+    (sym1: Symbol) (sym1Port1:Port) (sym1Port2:Port) 
+    (sym2: Symbol) (sym2Port1:Port) (sym2Port2:Port) =
+    let xy1= readPortPosition sym1 sym1Port1
+    let xy2= readPortPosition sym2 sym1Port2
+    let xy3= readPortPosition sym2 sym2Port1
+    let xy4= readPortPosition sym2 sym2Port2
+    // Determine alignment axis
+    let axis =
+        match Map.tryFind sym1Port1.Id sym1.PortMaps.Orientation, Map.tryFind sym2Port1.Id sym2.PortMaps.Orientation with
+        | Some(Top), Some(Bottom) | Some(Bottom), Some(Top) -> "Horizontal"
+        | Some(Left), Some(Right) | Some(Right), Some(Left) -> "Vertical"
+        | _ -> failwith "Invalid or unsupported port orientation combination"
+    
+    // Calculate new dimensions
+    let newSym1Dims, newSym2Dims =
+        match axis with
+        | "Horizontal" ->
+            // Calculate new dimensions/positions for horizontal alignment
+            let width1 = abs(xy2.X - xy1.X)
+            let width2 = abs(xy4.X - xy3.X)
+            match width1 < width2 with
+            | true -> 
+                let newWidthsym1 = (width2-width1)/width1 * sym1.Component.W + sym1.Component.W
+                let newHeightsym1 = sym1.Component.H
+                let newWidthsym2 = sym2.Component.W
+                let newHeightsym2 = sym2.Component.H
+                ((newWidthsym1,newHeightsym1), (newWidthsym2,newHeightsym2))
+            | false ->
+                let newWidthsym1 = sym1.Component.W
+                let newHeightsym1 = sym1.Component.H
+                let newWidthsym2 = (width1-width2)/width2 * sym2.Component.W + sym2.Component.W
+                let newHeightsym2 = sym2.Component.H
+                ((newWidthsym1,newHeightsym1), (newWidthsym2,newHeightsym2))
+
+        | "Vertical" ->
+            // Calculate new dimensions/positions for vertical alignment
+            let height1 = abs(xy2.Y - xy1.Y)
+            let height2 = abs(xy4.Y - xy3.Y)
+            match height1 < height2 with
+            | true -> 
+                let newWidthsym1 = sym1.Component.W
+                let newHeightsym1 = (height2-height1)/height1 * sym1.Component.H + sym1.Component.H
+                let newWidthsym2 = sym2.Component.W
+                let newHeightsym2 = sym2.Component.H
+                ((newWidthsym1,newHeightsym1), (newWidthsym2,newHeightsym2))
+            | false ->
+                let newWidthsym1 = sym1.Component.W
+                let newHeightsym1 = sym1.Component.H
+                let newWidthsym2 = sym2.Component.W
+                let newHeightsym2 = (height1-height2)/height2 * sym2.Component.H + sym2.Component.H
+                ((newWidthsym1,newHeightsym1), (newWidthsym2,newHeightsym2))
+        
+        | _ -> failwith "Invalid or unsupported axis"
+
+    // Apply changes using lenses
+    let sym1Option = Map.tryFind sym1.Id model.Symbols
+    let sym2Option = Map.tryFind sym2.Id model.Symbols
+
+    // Update dimensions if symbols are found
+    let updatedSymbols = model.Symbols
+                          |> Map.change sym1.Id (Option.map (fun sym -> snd customComponentDimensionsLens newSym1Dims sym))
+                          |> Map.change sym2.Id (Option.map (fun sym -> snd customComponentDimensionsLens newSym2Dims sym))
+
+    // Return updated model
+    { model with Symbols = updatedSymbols }
+
+
+// determine which two sides of two symbols have the most ports connected on the same axis

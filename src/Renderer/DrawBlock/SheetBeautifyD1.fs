f996b3eedeef12e990b2a6381c8024a363f05000
Aranya Gupta <ag1421@ic.ac.uk>
2024-03-10 21:17:07 +0000

    Start work on Appendix A Build option 3

--- src/Renderer/DrawBlock/SheetBeautifyD1.fs
+++ src/Renderer/DrawBlock/SheetBeautifyD1.fs
@@ -21,13 +21,81 @@ open Optics.Operators // for >-> operator
 let sheetAlignScale (sheet: SheetT.Model) = 
     // 1. Align all singly-connected components to eliminate wire bends in parallel wires
     let connectedSymbols = getConnSyms sheet.Wire
-    // find symbols that only have one connection (singly connected symbols)
-    let singlyConnected = 
+    
+    let removeCount pair = 
+        let s, m = fst pair, snd pair
+        List.map fst s, List.map fst m
+
+    // TODO: fix this - singly connected can have two output wires as long as only one of them is parallel
+    // find singly and multiply connected symbols (only connected to one symbol, connected to multiple symbols)
+    let singlyConnected, multiplyConnected = 
         connectedSymbols
         |> List.collect (fun (sym1, sym2) -> [sym1;sym2])
         |> List.countBy id
-        |> List.filter (fun (_, count) -> count=1)
-        |> List.map (fun (sym, _) -> sym)
+        |> List.partition (fun (_, count) -> count=1)
+        |> removeCount
+    
+    // 3. Where possible align multiply connected components to eliminate wire bends in nearly straight wires
+    // This is done first as there are more restrictions on how multiply connected components can move, 
+    // so adjusting them first allows us to remove some harder wire bends without affecting the easier ones
+
+    // Four cases:
+    // Case 1: 1 output, connected to 1 component (ie only multiply connected on input)
+    // Move component to the X or Y position of its output component
+    // Case 2: 1 output, connected to several components (A, B, C)
+    // Move component to the A's top port X or Y position 
+    // Will still have unstraightened parallel wires, but fewer
+    // Case 3: Several outputs, all connected to 1 component
+    // Move to X or Y position of top port of connected component
+    // Case 4: Several outputs, connected to several components (A, B, C)
+    // Move to X or Y position of top port of one of the components its connected to (A for example)
+    // Smart overlap checker for case 2&4 - if it overlaps with some other component move it to top port of B then C
+    // Difficult - get other cases working right first
+
+    // TODO: Do the same for vertical
+    // Case 1:
+
+    // TODO: Check whether we should use inputportid or outputportid
+    let getConnectingWire portAndSym = 
+        let port = fst portAndSym
+        sheet.Wire.Wires
+        |> mapValues
+        |> Seq.toList
+        |> List.filter (
+            fun wire -> 
+                (getSourcePort sheet.Wire wire).Id=port
+        )
+        |> List.tryExactlyOne
+        |> function
+        | None -> None, snd portAndSym
+        | Some wire ->Some (getTargetPort sheet.Wire wire).Id, snd portAndSym
+
+    let case1TargetPort = 
+        multiplyConnected
+        |> List.map (fun sym -> sym.PortMaps.Order[Right], sym)
+        |> List.filter (fun portList -> (fst portList).Length = 1)
+        |> List.map (fun (singleton, sym) -> singleton[0], sym)
+        |> List.map getConnectingWire 
+        |> List.filter (fun port -> (fst port).IsSome)
+        |> List.map (fun (tPort, sym) -> Option.get tPort, sym)
+    
+    let case1PortPosns = 
+        case1TargetPort
+        |> List.map fst 
+        |> List.map getPortPos 
+        |> List.map (fun curried -> curried sheet.Wire.Symbol)
+    
+    let case1UpdatedSymbols = 
+        case1PortPosns
+        |> List.map updateSymPos
+        |> List.mapi (fun i curried -> curried (snd case1TargetPort[i]))
+    
+    let case1UpdatedSheet = 
+        (sheet.Wire.Symbol.Symbols, case1UpdatedSymbols)
+        ||> List.fold (fun acc sym -> Map.add sym.Id sym acc)
+        |> Optic.set symbols_
+        <| sheet
+
     
     let isSinglyConnected (sym: Symbol) = List.tryFind (fun s -> sym=s) singlyConnected
     // find the singly connected symbol's port and its paired port (input-output or output-input)
@@ -52,9 +120,9 @@ let sheetAlignScale (sheet: SheetT.Model) =
                     let possiblePort = List.tryFind (fun (x: string) -> wire.InputPort=(InputPortId x)) allSymPorts
                     match possiblePort with
                     | None -> false
-                    | Some port -> true
+                    | Some _ -> true
             )
-            |> List.tryExactlyOne
+            |> List.tryExactlyOne // TODO: Change to first port so that it always aligns from the top
         match wireConnecting with 
         | None -> None, None
         | Some w -> (Some (getSourcePort sheet.Wire w), Some (getTargetPort sheet.Wire w))
@@ -63,7 +131,7 @@ let sheetAlignScale (sheet: SheetT.Model) =
 
     // Find the singly connected symbol and the symbol it is connected to, and 
     // move the singly connected symbol to the x/y coordinate of the port it is connected to
-    // Updates the enitre sheet
+    // Updates the entire sheet
     let updatedSheet = 
         connectedSymbols
         |> List.filter (
@@ -136,7 +204,7 @@ let sheetAlignScale (sheet: SheetT.Model) =
         |> Optic.set symbols_
         <| updatedSheet
 
-
+    
     // 2/7. Do not overlap components
     // Go through updated sheet and check which symbols overlap
     // if they overlap, move the symbol to its old position in the original sheet

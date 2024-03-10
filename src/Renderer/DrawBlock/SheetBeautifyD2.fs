7da2a6c7b42a6e09ceb352181e5582e65abd377f
F-Codewater <alexander.saul@hotmail.co.uk>
2024-03-10 17:38:12 +0000

    Cleaning SheetBeautifyD2 for clarity

--- src/Renderer/DrawBlock/SheetBeautifyD2.fs
+++ src/Renderer/DrawBlock/SheetBeautifyD2.fs
@@ -14,107 +14,6 @@ open Helpers
 open Symbol
 open BusWireRoute
 
-// /// Permutes gate inputs
-// let rec permute list =
-//     match list with
-//     | [] -> [ [] ]
-//     | head :: tail ->
-//         let rec insertEverywhere x list =
-//             match list with
-//             | [] -> [ [ x ] ]
-//             | head :: tail ->
-//                 (x :: list)
-//                 :: [ for rest in insertEverywhere x tail -> head :: rest ]
-//         let tailPerms = permute tail
-//         tailPerms |> List.collect (insertEverywhere head)
-
-// /// Applies all permutations of gate inputs and MUX inputs and evaluate the transformations to output the best model
-// let evaluateTransformations (model: Model) : Model =
-//     printf "BEGINNING EVALUATE TRANSFORMATIONS -------"
-    
-//     let originalCrossings = numOfWireRightAngleCrossings model
-//     let originalRightAngles = numOfVisRightAngles model
-
-//     // Extracts gates from the model
-//     let gates =
-//         model.Wire.Symbol.Symbols
-//         |> Map.filter (fun _ symbol ->
-//             match symbol.Component.Type with
-//             | GateN _ -> true
-//             | _ -> false)
-//         |> Map.toList
-//     printfn "Found %d gates" (List.length gates)
-//     gates |> List.iter (fun (id, sym) -> printfn "Gate Id: %A, Type: %A" id sym.Component.Type)
-
-//     // Extracts MUXes from the model
-//     let muxes =
-//         model.Wire.Symbol.Symbols
-//         |> Map.filter (fun _ symbol ->
-//             match symbol.Component.Type with
-//             | Mux2 -> true
-//             | _ -> false)
-//         |> Map.toList
-//     printfn "Found %d MUXes" (List.length muxes)
-//     muxes |> List.iter (fun (id, sym) -> printfn "MUX Id: %A, Reversed Inputs: %A" id sym.ReversedInputPorts)
-//     muxes
-//     |> List.iter (fun (_, symbol) ->
-//             symbol.Component.InputPorts
-//             |> List.iter (fun port ->
-//                 printfn "MUX portId: %A portNumber: %A, portType: %A" port.Id port.PortNumber port.PortType))
-    
-
-//     // Generates all permutations for gates
-//     let gatePermutations =
-//         gates
-//         |> List.collect (fun (id, symbol) ->
-//             let inputPerms = permute symbol.Component.InputPorts
-//             printfn "Gate Id: %A permutations:" id
-//             inputPerms |> List.iter (fun perm -> printfn "%A" perm)
-//             inputPerms
-//             |> List.map (fun perm -> (id, { symbol with Component = { symbol.Component with InputPorts = perm } })))
-//     printfn "Generated %d gate permutations" (List.length gatePermutations)
-
-//     // Generates all flipped MUX models
-//     let muxPermutations =
-//         muxes
-//         |> List.map (fun (id, symbol) ->
-//             match symbol.ReversedInputPorts with
-//             | Some state ->
-//                 let flippedSymbol = Optic.set reversedInputPorts_ (Some(not state)) symbol
-//                 // Print the flipped state of each MUX
-//                 printfn "Flipped MUX: %A" flippedSymbol
-//                 (id, flippedSymbol)
-//             | None -> failwithf "Reversed Input Ports not found for MUX %A" id)
-//     printfn "Generated %d MUX permutations" (List.length muxPermutations)
-
-//     // Applies permutations and flip transformations
-//     let models =
-//         gatePermutations @ muxPermutations
-//         |> List.map (fun (id, symbol) ->
-//             { model with
-//                 Wire =
-//                     { model.Wire with
-//                         Symbol =
-//                             { model.Wire.Symbol with
-//                                 Symbols = model.Wire.Symbol.Symbols |> Map.add id symbol } } })
-
-//     // Evaluates each model and select the best one
-//     let bestModel =
-//         models
-//         |> List.minBy (fun m ->
-//             let crossings = numOfWireRightAngleCrossings m
-//             let rightAngles = numOfVisRightAngles m
-//             if
-//                 crossings <= originalCrossings
-//                 && rightAngles <= originalRightAngles
-//             then
-//                 crossings, -rightAngles
-//             else
-//                 failwithf "cannot find best model")
-
-//     bestModel
-
-
 // --------------------------------------------------------------
 // --------------------------------------------------------------
 // D2 using only horizontal flips, permuting across every option 
@@ -164,7 +63,7 @@ let flipAndRerouteComp (model: SheetT.Model) (compId: ComponentId) : SheetT.Mode
     
 
 /// Find the better performing model based off total wire crossings and right angles
-let evaluateModels (flippedModel: SheetT.Model) (originalModel: SheetT.Model) = 
+let evaluateModels (flippedModel: SheetT.Model) (originalModel: SheetT.Model) : SheetT.Model = 
     let originalCrossings = numOfWireRightAngleCrossings originalModel
     let originalRightAngles = numOfVisRightAngles originalModel    
     let newCrossings = numOfWireRightAngleCrossings flippedModel
@@ -239,23 +138,4 @@ let findBestModel (model: SheetT.Model) : SheetT.Model =
 //------------------------------------------------------------
 
 
-// let reverseAllMuxInputPorts (model: Model) : Model =
-//     let reverseInputPort symbol =
-//         let newSymbol =
-//             match symbol.ReversedInputPorts with
-//             | Some state -> Optic.set reversedInputPorts_ (Some(not state)) symbol
-//             | None -> Optic.set reversedInputPorts_ (Some true) symbol // Default to true if not set
-//         newSymbol.Component.InputPorts
-//         |> List.iter (fun port ->
-//         newSymbol
-
-//     let updatedSymbols =
-//         model.Wire.Symbol.Symbols
-//         |> Map.map (fun _ symbol ->
-//             match symbol.Component.Type with
-//             | Mux2 -> 
-//                 reverseInputPort symbol
-//             | _ -> symbol) // Skip non-MUX2 symbols
-
-//     { model with Wire = { model.Wire with Symbol = { model.Wire.Symbol with Symbols = updatedSymbols } } }
 

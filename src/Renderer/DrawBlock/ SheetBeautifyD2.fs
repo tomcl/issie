6eada24e27357a404da7010fef26ed68a951faf8
Johnny18823 <jw3621@ic.ac.uk>
2024-03-14 14:31:08 +0000

    complete brute force algo

--- src/Renderer/DrawBlock/ SheetBeautifyD2.fs
+++ src/Renderer/DrawBlock/ SheetBeautifyD2.fs
@@ -1,4 +1,4 @@
-module SheetBeautify
+module SheetBeautifyD2
 
 //-----------------------------------------------------------------------------------------//
 //----------Module for top-level beautify functions making (mostly) whole-sheet changes----//
@@ -28,112 +28,113 @@ open SheetBeautifyHelpers
 open Optics
 
 /// constants used by SheetBeautify
-module SheetBeautifyD2 =
-    // D2. sheetOrderFlip 
-    // Adjust on sheet: Port order on custom components, flip components, flip MUX input order
-    // Primary Optimisation: Reduce wire crossings 
-    // Test using random sample inputsReduction in wire crossings, other quality measures
+module Constants =
+    ()
+// D2. sheetOrderFlip 
+// Adjust on sheet: Port order on custom components, flip components, flip MUX input order
+// Primary Optimisation: Reduce wire crossings 
+// Test using random sample inputsReduction in wire crossings, other quality measures
 
-    // Build Function: 
-    //      - try through all possible combinations of flipping components
-    //      - find the one with the least wire crossings
-    //      - apply that combination to the model and return the new model
+// Build Function: 
+//      - try through all possible combinations of flipping components
+//      - find the one with the least wire crossings
+//      - apply that combination to the model and return the new model
 
-    // script to describe a symbol
-    type symbolScript = {
-        Flipped: bool
-        ReversedInput: bool
-        PortEdge: Edge
-        PortOrder: list<string>
-    }
+// script to describe a symbol
+type symbolScript = {
+    Flipped: bool
+    ReversedInput: bool
+    PortEdge: Edge
+    PortOrder: list<string>
+}
 
-    // script to describe a model
-    type modelScript = list<symbolScript>
+// script to describe a model
+type modelScript = list<symbolScript>
 
-    let rec combinations list =
-        match list with
-        | [] -> [[]] // The only combination of an empty list is a list with an empty list
-        | head :: tail ->
-            let recSubsets = combinations tail
-            recSubsets @ (recSubsets |> List.map (fun subset -> head :: subset))
+let rec combinations list =
+    match list with
+    | [] -> [[]] // The only combination of an empty list is a list with an empty list
+    | head :: tail ->
+        let recSubsets = combinations tail
+        recSubsets @ (recSubsets |> List.map (fun subset -> head :: subset))
+
+let generateSymbolScript (symbol: SymbolT.Symbol) =
+    let flips = [true; false]
+    let reversedInputs = [true; false]
+    let portEdges = [Edge.Left; Edge.Right; Edge.Top; Edge.Bottom]
     
-    let generateSymbolScript (symbol: SymbolT.Symbol) =
-        let flips = [true; false]
-        let reversedInputs = [true; false]
-        let portEdges = [Edge.Left; Edge.Right; Edge.Top; Edge.Bottom]
-        
-        let portOrderCombs = 
-            portEdges
-            |> List.map (fun edge -> symbol.PortMaps.Order[edge])
-            |> combinations
-        
-        let allCombs =
-            List.collect (fun flipped ->
-                List.collect (fun reversedInput ->
-                    List.map (fun (portEdge, portOrder) ->
-                        {
-                            Flipped = flipped
-                            ReversedInput = reversedInput
-                            PortEdge = portEdge
-                            PortOrder = []
-                        }
-                    ) (List.zip portEdges portOrderCombs)
-                ) reversedInputs
-            ) flips
-        
-        allCombs
+    let portOrderCombs = 
+        portEdges
+        |> List.map (fun edge -> symbol.PortMaps.Order[edge])
+        |> combinations
     
-    let generateModelScript (model: SheetT.Model): list<modelScript> =
-        let symbols = 
-            model.Wire.Symbol.Symbols
-            |> Map.toList
-            |> List.map snd
-        ()
-        //TODO: Implement this
-        
-
-    // update a symbol with a symbolScript
-    let applyScript (script: symbolScript) (symbol: SymbolT.Symbol) =
-        let _, updatePortOrder = symPortOrder_ script.PortEdge
-        let _, updateMux2InputOrder = reverseMux2Input_
-        let _, updateFlip = symbolFlipped_
-
-        symbol
-        |> updatePortOrder (Some script.PortOrder) 
-        |> updateMux2InputOrder script.ReversedInput
-        |> updateFlip script.Flipped
+    let allCombs =
+        List.collect (fun flipped ->
+            List.collect (fun reversedInput ->
+                List.map (fun (portEdge, portOrder) ->
+                    {
+                        Flipped = flipped
+                        ReversedInput = reversedInput
+                        PortEdge = portEdge
+                        PortOrder = []
+                    }
+                ) (List.zip portEdges portOrderCombs)
+            ) reversedInputs
+        ) flips
     
+    allCombs
+
+let generateModelScript (model: SheetT.Model): list<modelScript> =
+    let symbols = 
+        model.Wire.Symbol.Symbols
+        |> Map.toList
+        |> List.map snd
+    ()
+    //TODO: Implement this
+    
+
+// update a symbol with a symbolScript
+let applyScript (script: symbolScript) (symbol: SymbolT.Symbol) =
+    let _, updatePortOrder = symPortOrder_ script.PortEdge
+    let _, updateMux2InputOrder = reverseMux2Input_
+    let _, updateFlip = symbolFlipped_
+
+    symbol
+    |> updatePortOrder (Some script.PortOrder) 
+    |> updateMux2InputOrder script.ReversedInput
+    |> updateFlip script.Flipped
+
+// TODO: Apply the script to each symbol
+let applyScriptToModel (model: SheetT.Model) (script: modelScript): SheetT.Model =
+    let symbols = 
+        model.Wire.Symbol.Symbols
+        |> Map.toList
+        |> List.map snd
     // TODO: Apply the script to each symbol
-    let applyScriptToModel (model: SheetT.Model) (script: modelScript): SheetT.Model =
-        let symbols = 
-            model.Wire.Symbol.Symbols
-            |> Map.toList
-            |> List.map snd
-        // TODO: Apply the script to each symbol
-        ()
+    ()
 
-    // Count the number of right angle intersections
-    let evaluateModel (model: SheetT.Model) =
-        countTotalRightAngleIntersect model
+// Count the number of right angle intersections
+let evaluateModel (model: SheetT.Model) =
+    countTotalRightAngleIntersect model
 
-    // Get optimized model
-    let optimizeFlipForComponents (model: SheetT.Model): SheetT.Model =
-        // Convert symbol map to list
-        let scripts = generateModelScript model
+// Get optimized model
+let optimizeFlipForComponents (model: SheetT.Model): SheetT.Model =
+    // Convert symbol map to list
+    let scripts = generateModelScript model
 
-        let modelsWithScores = 
-            scripts
-            |> List.map (applyScriptToModel model)
-            |> List.map (fun model -> (model, evaluateModel model))
+    let modelsWithScores = 
+        scripts
+        |> List.map (applyScriptToModel model)
+        |> List.map (fun model -> (model, evaluateModel model))
 
-        let bestModel = 
-            modelsWithScores
-            |> List.minBy snd
-            |> fst
-        
-        bestModel
-        
+    let bestModel = 
+        modelsWithScores
+        |> List.minBy snd
+        |> fst
     
-    // Heuristic Algorithm
-    // Use a heuristic to partition components into independent connected groups
-    // TODO
+    bestModel
+    
+
+// Heuristic Algorithm
+// Use a heuristic to partition components into independent connected groups
+// TODO
--- src/Renderer/DrawBlock/SheetBeautify.fs
+++ src/Renderer/DrawBlock/SheetBeautify.fs
@@ -1,4 +1,4 @@
-﻿module SheetBeautify
+﻿module SheetBeautifyD2
 
 //-----------------------------------------------------------------------------------------//
 //----------Module for top-level beautify functions making (mostly) whole-sheet changes----//
@@ -30,3 +30,144 @@ open Optics
 /// constants used by SheetBeautify
 module Constants =
     ()
+// D2. sheetOrderFlip 
+// Adjust on sheet: Port order on custom components, flip components, flip MUX input order
+// Primary Optimisation: Reduce wire crossings 
+// Test using random sample inputsReduction in wire crossings, other quality measures
+
+// Build Function: 
+//      - try through all possible combinations of flipping components
+//      - find the one with the least wire crossings
+//      - apply that combination to the model and return the new model
+
+// script to describe a symbol
+type symbolScript = {
+    Flipped: bool
+    ReversedInput: bool
+    PortEdge: Edge
+    PortOrder: list<string>
+}
+
+// script to describe a model
+type modelScript = list<ComponentId * symbolScript>
+
+let rec combinations list =
+    match list with
+    | [] -> [[]] // The only combination of an empty list is a list with an empty list
+    | head :: tail ->
+        let recSubsets = combinations tail
+        recSubsets @ (recSubsets |> List.map (fun subset -> head :: subset))
+
+let generateSymbolScript (symbol: SymbolT.Symbol) =
+    let flips = [true; false]
+    let reversedInputs = [true; false]
+    let portEdges = [Edge.Left; Edge.Right; Edge.Top; Edge.Bottom]
+    
+    let portOrderCombs = 
+        portEdges
+        |> List.map (fun edge -> symbol.PortMaps.Order[edge])
+        |> combinations
+    
+    let allCombs =
+        List.collect (fun flipped ->
+            List.collect (fun reversedInput ->
+                List.map (fun (portEdge, portOrder) ->
+                    {
+                        Flipped = flipped
+                        ReversedInput = reversedInput
+                        PortEdge = portEdge
+                        PortOrder = []
+                    }
+                ) (List.zip portEdges portOrderCombs)
+            ) reversedInputs
+        ) flips
+    
+    allCombs
+
+let generateModelScript (model: SheetT.Model): list<modelScript> =
+    let symbols = 
+        model.Wire.Symbol.Symbols
+        |> Map.toList
+        |> List.map snd
+    
+    let Ids = 
+        model.Wire.Symbol.Symbols
+        |> Map.toList
+        |> List.map fst
+    
+    let symbolScriptLists = symbols |> List.map generateSymbolScript
+    
+    let rec cartesianProduct lists =
+        match lists with
+        | [] -> [[]]
+        | head::tail ->
+            let rec combineWithAll x list =
+                match list with
+                | [] -> []
+                | head::tail -> (x::head) :: combineWithAll x tail
+            let tailProduct = cartesianProduct tail
+            head |> List.collect (fun x -> combineWithAll x tailProduct)
+            
+    // Generating all possible combinations
+    symbolScriptLists
+    |> cartesianProduct
+    |> List.map (fun scripts -> List.zip Ids scripts)
+
+
+
+// update a symbol with a symbolScript
+let applyScriptToSymbol (script: symbolScript) (symbol: SymbolT.Symbol) =
+    let _, updatePortOrder = symPortOrder_ script.PortEdge
+    let _, updateMux2InputOrder = reverseMux2Input_
+    let _, updateFlip = symbolFlipped_
+
+    symbol
+    |> updatePortOrder (Some script.PortOrder) 
+    |> updateMux2InputOrder script.ReversedInput
+    |> updateFlip script.Flipped
+
+// TODO: Apply the script to each symbol
+let applyScriptToModel (model: SheetT.Model) (modelScript: modelScript): SheetT.Model =
+    
+    let updateOneSymbol (symScript: symbolScript)(symId: ComponentId)(model: SheetT.Model): SheetT.Model = 
+        let applyScriptToSymbol' = applyScriptToSymbol symScript
+        
+        let newSymModel: SymbolT.Model = 
+            SymbolUpdate.updateSymbol applyScriptToSymbol' symId model.Wire.Symbol
+
+        let newModel = 
+            model
+            |> Optic.set symbol_ newSymModel
+            |> updateBoundingBoxes
+        
+        newModel
+    
+    modelScript
+    |> List.fold (fun model (symId, symScript) -> updateOneSymbol symScript symId model) model
+
+
+// Count the number of right angle intersections
+let evaluateModel (model: SheetT.Model) =
+    countTotalRightAngleIntersect model
+
+// Get optimized model
+let optimizeFlipForComponents (model: SheetT.Model): SheetT.Model =
+    // Convert symbol map to list
+    let scripts = generateModelScript model
+
+    let modelsWithScores = 
+        scripts
+        |> List.map (applyScriptToModel model)
+        |> List.map (fun model -> (model, evaluateModel model))
+
+    let bestModel = 
+        modelsWithScores
+        |> List.minBy snd
+        |> fst
+    
+    bestModel
+    
+
+// Heuristic Algorithm
+// Use a heuristic to partition components into independent connected groups
+// TODO

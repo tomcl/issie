c898449c3cbca94ad84ff9c0d3b8781c12788d7a
Harry Ni <harryniyh@hotmail.com>
2024-03-16 13:30:19 +0000

    add makeRandomCircuit; define DU TestCompType to use more DSLs; refactor generators and helpers in a module

--- src/Renderer/TestDrawBlockD2.fs
+++ src/Renderer/TestDrawBlockD2.fs
@@ -22,6 +22,7 @@ module D2Test =
     open DrawModelType
     open Sheet.SheetInterface
 
+
 //------------------------------------------------------------------------------------------------------------------------//
 //------------------------------additional metrics to assess improvements-------------------------------------------//
 //------------------------------------------------------------------------------------------------------------------------//
@@ -244,15 +245,16 @@ module D2Test =
             |> SheetUpdateHelpers.updateBoundingBoxes
 
         // Flip a symbol
-        let flipSymbol (symLabel: string) (flip: SymbolT.FlipType) (model: SheetT.Model) : (SheetT.Model) =
+        let flipSymbol (symLabel: string) (flip: SymbolT.FlipType option) (model: SheetT.Model) : (SheetT.Model) =
             let symId = getSymId symLabel model.Wire.Symbol
             let flipSymbol' = 
                 match flip with
-                | SymbolT.FlipType.FlipHorizontal -> 
+                | Some SymbolT.FlipType.FlipHorizontal -> 
                     SymbolResizeHelpers.flipSymbol SymbolT.FlipType.FlipHorizontal
-                | SymbolT.FlipType.FlipVertical ->
+                | Some SymbolT.FlipType.FlipVertical ->
                     SymbolResizeHelpers.flipSymbol SymbolT.FlipType.FlipHorizontal 
                     >> SymbolResizeHelpers.rotateAntiClockByAng Degree180 
+                | None -> id
             let symModel: SymbolT.Model = 
                 SymbolUpdate.updateSymbol flipSymbol' symId model.Wire.Symbol
 
@@ -261,71 +263,153 @@ module D2Test =
             |> SheetUpdateHelpers.updateBoundingBoxes
 
         // ----------------------Circuit Builders DSL----------------------
+        type TestCompType = 
+        | TAnd
+        | TOr
+        | TNot
+        | TMux2
+        | TMux4
+        | TInput
+        | TOutput
+        | TCustomComp
+
         let minimalDSL (initSheetModel: SheetT.Model) (placers: (SheetT.Model -> Result<SheetT.Model,'a>) list) : Result<SheetT.Model,'a> = 
             (Ok initSheetModel, placers)
             ||> List.fold (fun circuitRes placer -> Result.bind placer circuitRes)
+
+        let initCCSheet =
+            [
+                placeSymbol "S1" (Input1(1, None)) {X=middleOfSheet.X-150.; Y=middleOfSheet.Y};
+                placeSymbol "S2" (Input1(1, None)) {X=middleOfSheet.X-150.; Y=middleOfSheet.Y+100.};
+                placeSymbol "S3" (Input1(1, None)) {X=middleOfSheet.X-150.; Y=middleOfSheet.Y+200.};
+                placeSymbol "S4" (Input1(1, None)) {X=middleOfSheet.X-150.; Y=middleOfSheet.Y+300.};
+                placeSymbol "X1" (Output(1)) {X=middleOfSheet.X+150.; Y=middleOfSheet.Y};
+                placeSymbol "X2" (Output(1)) {X=middleOfSheet.X+150.; Y=middleOfSheet.Y+10.};
+                placeSymbol "X3" (Output(1)) {X=middleOfSheet.X+150.; Y=middleOfSheet.Y+20.};
+                placeSymbol "X4" (Output(1)) {X=middleOfSheet.X+150.; Y=middleOfSheet.Y+300.};
+            ]
+            |> minimalDSL initSheetModel
+            |> getOkOrFail
+
+        /// DSL for symbol placer, automatically names the symbol using a given index
+        let symPlacerDSL (idx: int) (compType: TestCompType) (pos: XYPos) (model: Model) (sheetModel: SheetT.Model) : Result<SheetT.Model, string> =
+            match compType with
+            | TAnd -> placeSymbol (sprintf "S%d" idx) (GateN(And,2)) pos sheetModel
+            | TOr -> placeSymbol (sprintf "S%d" idx) (GateN(Or,2)) pos sheetModel
+            | TNot -> placeSymbol (sprintf "S%d" idx) (Not) pos sheetModel
+            | TMux2 -> placeSymbol (sprintf "S%d" idx) Mux2 pos sheetModel
+            | TMux4 -> placeSymbol (sprintf "S%d" idx) Mux4 pos sheetModel
+            | TInput -> placeSymbol (sprintf "S%d" idx) (Input1(1, None)) pos sheetModel
+            | TOutput -> placeSymbol (sprintf "S%d" idx) (Output(1)) pos sheetModel
+            | TCustomComp -> placeCustomSymbol (sprintf "S%d" idx) initCCSheet "custom" model {X=1.0; Y=1.0} pos sheetModel
+            | _ -> Ok sheetModel // do nothing
+            // TODO: input and output symbols have only one of input/output port
+            // need an algorithm to conenct them
+
+        let wirePlacerDSL (sourceSymLabel: string) (targetSymLabel: string) (sourceIdx) (targetIdx) (sheetModel: SheetT.Model) : Result<SheetT.Model, string> =
+            sheetModel
+            |> placeWire (portOf sourceSymLabel sourceIdx) (portOf targetSymLabel targetIdx) 
+
         
             
 //--------------------------------------------------------------------------------------------------//
-//----------------------------------------Example Test Circuits using Gen<'a> samples---------------//
+//---------------------------------------- Predefined Gen<'a> and Helpers---------------------------//
 //--------------------------------------------------------------------------------------------------//
+    module Generator =
+        open System
+        open Builder
+
+        // ---------------------- Generator Helpers ----------------------
+        let makeTuple a b = (a, b)
+
+        /// From Ed: Fischer-Yates shuffle algorithm
+        /// Returns a random shuffled array without changing the input array
+        let shuffleAGen (rng: System.Random) arrayToShuffle: 'a array =
+            let tmpA = Array.copy arrayToShuffle
+            for i = 0 to tmpA.Length - 1 do 
+                let r = rng.Next(i, tmpA.Length);
+                (tmpA[i],tmpA[r])
+                |> fun (iv, rv) -> tmpA[r] <- iv;  tmpA[i]  <- rv
+            tmpA
+
+        /// Chunk the generator into smaller arrays of equal lengths
+        let chunkShuffledGen (length: int) (seed: int) (gen: Gen<'a>)  =
+            gen
+            |> toArray
+            |> shuffleAGen (Random seed) // random by test number
+            |> Array.chunkBySize length
+            |> fromArray
+
+        // ====================== Positions Generator ======================
+
+        /// Sample data based on 11 equidistant points on a horizontal line
+        let horizLinePositions =
+            fromList [-100..20..100]
+            |> map (fun n -> middleOfSheet + {X=float n; Y=0.})
+
+        let gridPositions n : Gen<XYPos> =
+            let genX = fromList [-n..20..n]
+            let genY = fromList [-n..20..n]
+            (genX, genY)
+            ||> product (fun x y -> middleOfSheet + {X=float x; Y=float y})
+
+        let gridPosGen (n: int) (interval: int) : Gen<XYPos> =
+            let genX: Gen<int> = fromList [-n..interval..n]
+            let genY = fromList [-n..interval..n]
+            (genX, genY)
+            ||> product (fun x y -> middleOfSheet + {X=float x; Y=float y})
+
+        let filteredGridPositions (sheetMaker) (pos: int) =
+            let existOverlapWithBoxes (pos: XYPos) =
+                let sheet: SheetT.Model = sheetMaker pos
+                let boxes: (int * BoundingBox) list = // list<(int * bounding box)>
+                    sheet.BoundingBoxes
+                    |> mapValues
+                    |> Array.toList
+                    |> List.mapi (fun n (box: BoundingBox) -> n,box)
+
+                boxes
+                |> List.allPairs boxes
+                |> List.exists (fun ((n1,box1),(n2,box2)) -> (n1 <> n2) && BlockHelpers.overlap2DBox box1 box2)
+
+            GenerateData.filter (existOverlapWithBoxes >> not) (gridPositions 100)
+
+        /// Gen samples incorporating two flips
+        let flipOnlySamples: Gen<{| AndPos: XYPos; Flip1: SymbolT.FlipType option; Flip2: SymbolT.FlipType option |}> =
+            let flips = fromList [Some SymbolT.FlipType.FlipHorizontal; Some SymbolT.FlipType.FlipVertical; None]
+            gridPositions 100
+            |> product makeTuple flips  
+            |> product makeTuple flips
+            // This final stage makes the output more readable in an anonymous record
+            |> map (fun (flip1, (flip2, (andPos)))->
+                {|
+                    Flip1=flip1;
+                    Flip2=flip2;
+                    AndPos=andPos
+                |})
+
+        // Gen samples incorporating a set of components
+        let randomComponentSamples: Gen<{| Comp: TestCompType; Flip: SymbolT.FlipType option; Pos: XYPos |}> =
+            let flips = fromList [Some SymbolT.FlipType.FlipHorizontal; Some SymbolT.FlipType.FlipVertical; None]
+            let comps = fromList [TAnd; TOr; TNot; TMux2; TMux4; TCustomComp] // TODO: TInput; TOutput; not yet supported
+
+            let tupleCompPosFlip: Gen<{| Comp: TestCompType; Pos: XYPos; Flip: SymbolT.FlipType option |}> =
+                (gridPosGen 200 100, flips)
+                ||> product makeTuple
+                |> product makeTuple comps
+                |> map (fun (comp, (pos, flip)) -> 
+                        {|
+                            Comp=comp;
+                            Pos=pos;
+                            Flip=flip;
+                        |}
+                )
+            tupleCompPosFlip
+        
     open Builder
-
-    // ====================== Positions Generator ======================
-
-    /// Sample data based on 11 equidistant points on a horizontal line
-    let horizLinePositions =
-        fromList [-100..20..100]
-        |> map (fun n -> middleOfSheet + {X=float n; Y=0.})
-
-    let gridPositions n : Gen<XYPos> =
-        let genX = fromList [-n..20..n]
-        let genY = fromList [-n..20..n]
-        (genX, genY)
-        ||> product (fun x y -> middleOfSheet + {X=float x; Y=float y})
-
-    let filteredGridPositions (sheetMaker) (pos: int) =
-        let existOverlapWithBoxes (pos: XYPos) =
-            let sheet: SheetT.Model = sheetMaker pos
-            let boxes: (int * BoundingBox) list = // list<(int * bounding box)>
-                sheet.BoundingBoxes
-                |> mapValues
-                |> Array.toList
-                |> List.mapi (fun n (box: BoundingBox) -> n,box)
-
-            boxes
-            |> List.allPairs boxes
-            |> List.exists (fun ((n1,box1),(n2,box2)) -> (n1 <> n2) && BlockHelpers.overlap2DBox box1 box2)
-
-        GenerateData.filter (existOverlapWithBoxes >> not) (gridPositions 100)
-
-    let makeTuple a b = (a, b)
-
-    /// Gen samples incorporating two sets of rotations, and two of flips
-    let flipOnlySamples =
-        let flips = fromList [Some SymbolT.FlipType.FlipHorizontal; Some SymbolT.FlipType.FlipVertical; None]
-        gridPositions 100
-        |> product makeTuple flips  
-        |> product makeTuple flips
-        // This final stage makes the output more readable in an anonymous record
-        |> map (fun (flipDFF, (flipAnd, (andPos)))->
-            {|
-                FlipDFF=flipDFF;
-                FlipAnd=flipAnd;
-                AndPos=andPos
-             |})
-
-    /// From Ed: Fischer-Yates shuffle algorithm
-    /// Returns a random shuffled array without changing the input array
-    let shuffleAGen (rng: System.Random) arrayToShuffle: 'a array =
-        let tmpA = Array.copy arrayToShuffle
-        for i = 0 to tmpA.Length - 1 do 
-            let r = rng.Next(i, tmpA.Length);
-            (tmpA[i],tmpA[r])
-            |> fun (iv, rv) -> tmpA[r] <- iv;  tmpA[i]  <- rv
-        tmpA
-
+    open Generator
     // ====================== Fixed Circuit Generator ======================
+
     let makeTest1Circuit (andPos:XYPos) =
         [
             placeSymbol "G1" (GateN(And,2)) {X=middleOfSheet.X+100.;Y=middleOfSheet.Y-100.};
@@ -333,7 +417,7 @@ module D2Test =
             placeSymbol "S2" (Input1(1, None)) {X=middleOfSheet.X-150.;Y=middleOfSheet.Y+100.};
             placeSymbol "MUX1" Mux2 {X=middleOfSheet.X-100.;Y=middleOfSheet.Y-100.};
             placeSymbol "MUX2" Mux2 middleOfSheet;
-            flipSymbol "MUX2" SymbolT.FlipType.FlipVertical >> Ok;
+            flipSymbol "MUX2" (Some SymbolT.FlipType.FlipVertical) >> Ok;
             placeWire (portOf "S2" 0) (portOf "MUX2" 2);
             placeWire (portOf "MUX1" 0) (portOf "MUX2" 0);
             placeWire (portOf "S1" 0) (portOf "MUX2" 1);
@@ -394,29 +478,39 @@ module D2Test =
         |> Result.bind (placeWire (portOf "MUX1" 0) (portOf "G1" 0) )
         |> getOkOrFail
 
-    let makeTest3Circuit (andPos:XYPos) =
-        initSheetModel
-        |> placeSymbol "G1" (GateN(And,2)) andPos
-        |> Result.bind (placeSymbol "FF1" DFF middleOfSheet)
-        |> Result.bind (placeWire (portOf "G1" 0) (portOf "FF1" 0))
-        |> Result.bind (placeWire (portOf "FF1" 0) (portOf "G1" 0) )
-        |> Result.bind (placeWire (portOf "FF1" 0) (portOf "G1" 1) )
-        |> getOkOrFail
+    let makeRandomCircuit (model:Model) (samples: {| Comp: TestCompType; Pos: XYPos; Flip: SymbolT.FlipType option |} array) =
+        let folder acc (sample: {| Comp: TestCompType; Pos: XYPos; Flip: SymbolT.FlipType option |}) = 
+            let (sheetModel, idx) = acc
+            let comp, pos, flip = sample.Comp, sample.Pos, sample.Flip
+            let newSheet =
+                [
+                    symPlacerDSL idx comp pos model;
+                    flipSymbol (sprintf "S%d" idx) flip >> Ok;
+                    if idx > 0 then wirePlacerDSL (sprintf "S%d" (idx)) (sprintf "S%d" (idx-1)) 0 0
+                    else id >> Ok;
+                ]
+                |> minimalDSL sheetModel
+                |> getOkOrFail
+            (newSheet, idx+1)
+        
+        samples
+        |> Array.fold folder (initSheetModel, 0)
+        |> fst
 
     // ====================== Random Flip Generator ======================
     let makeTest4Circuit (sample: {|
-        FlipDFF: SymbolT.FlipType option;
-        FlipAnd: SymbolT.FlipType option;
+        Flip1: SymbolT.FlipType option;
+        Flip2: SymbolT.FlipType option;
         AndPos: XYPos
     |}) =
         initSheetModel
         |> placeSymbol "G1" (GateN(And,2)) sample.AndPos
-        |> match sample.FlipAnd with
-            | Some f -> Result.map (flipSymbol "G1" SymbolT.FlipType.FlipHorizontal)
+        |> match sample.Flip2 with
+            | Some f -> Result.map (flipSymbol "G1" (Some SymbolT.FlipType.FlipHorizontal))
             | None -> id
         |> Result.bind (placeSymbol "FF1" DFF middleOfSheet)
-        |> match sample.FlipDFF with
-            | Some f -> Result.map (flipSymbol "G1" SymbolT.FlipType.FlipHorizontal)
+        |> match sample.Flip1 with
+            | Some f -> Result.map (flipSymbol "G1" (Some SymbolT.FlipType.FlipHorizontal))
             | None -> id
         |> Result.bind (placeWire (portOf "G1" 0) (portOf "FF1" 0))
         |> Result.bind (placeWire (portOf "FF1" 0) (portOf "G1" 0) )
@@ -468,35 +562,53 @@ module D2Test =
             |> recordPositionInTest testNum dispatch
 
         let test2 testNum firstSample dispatch model =
+            let sheetMaker = makeTest2Circuit model
             let displayOnFail = displayAll
-            let generator = filteredGridPositions makeTest3Circuit 10
+            let generator = filteredGridPositions sheetMaker 10
             runTestOnSheets
                 "DisplayAll: Custom Symbol"
                 firstSample
                 generator
-                (makeTest2Circuit model)
+                sheetMaker
                 Asserts.failOnAllTests
                 dispatch
                 displayOnFail
             |> recordPositionInTest testNum dispatch
 
         let test2Opt testNum firstSample dispatch model =
+            let sheetMaker = makeTest2Circuit model
             let displayOnFail = displayAll
-            let generator = filteredGridPositions makeTest3Circuit 10
+            let generator = filteredGridPositions sheetMaker 10
             runTestOnSheets
                 "DisplayAll: Custom Symbol"
                 firstSample
                 generator
-                (makeTest2Circuit model >> optimizePortOrder)
+                (sheetMaker >> optimizePortOrder)
                 Asserts.failOnAllTests
                 dispatch
                 displayOnFail
             |> recordPositionInTest testNum dispatch
 
+
+        let rec groupNElements n (lst: 'a list) =
+            let rec groupNElementsHelper acc currList remainingList =
+                match currList, remainingList with
+                | _, [] -> List.rev acc // Reverse the accumulator to maintain the order
+                | _, remainder when List.length remainder < n -> List.rev (remainder :: acc) // Add the remaining elements to the accumulator and reverse it
+                | _, _ ->
+                    let group, rest = List.splitAt n remainingList // Split the remaining list into a group of size n and the rest
+                    groupNElementsHelper (group :: acc) group rest // Recur with the updated accumulator and remaining list
+            groupNElementsHelper [] [] lst
+
         let test4 testNum firstSample dispatch model =
             let displayOnFail = displayMetric
-            let generator = flipOnlySamples
             let myRandomSample = shuffleAGen <| Random(1)
+            let generator = 
+                flipOnlySamples
+                |> toArray
+                |> myRandomSample
+                |> fromArray
+            
             runTestOnSheets
                 "DisplayAll: Random Flip DFF+AND"
                 firstSample
@@ -506,6 +618,31 @@ module D2Test =
                 dispatch
                 displayOnFail
             |> recordPositionInTest testNum dispatch
+
+        
+
+        let testRandomComp testNum firstSample dispatch model =
+            let sheetMaker = makeRandomCircuit model
+            let displayOnFail = displayAll
+            let generator = chunkShuffledGen 4 1 randomComponentSamples
+            // this assertion fails on all tests without symbol intersection!
+            let assertion (sample: int) (sheetModel: SheetT.Model) = 
+                sheetModel
+                |> Asserts.failOnSymbolIntersectsSymbol sample
+                |> function
+                    | Some str -> None
+                    | None -> Some "Random Component Test Failed"
+            
+            runTestOnSheets
+                "DisplayAll: Random 4 Components"
+                firstSample
+                generator
+                sheetMaker
+                assertion
+                dispatch
+                displayOnFail
+            |> recordPositionInTest testNum dispatch
+
 
         /// This test is to display info on the current model as it is in Issie
         let displayCurProj testNum firstSample dispatch model =
@@ -522,7 +659,7 @@ module D2Test =
                 "Test1: Optimised", test1Opt
                 "Test2: CC Original", test2
                 "Test2: CC Optimised", test2Opt
-                // "Test4: Random Flip", test4
+                "Test: Random Components", testRandomComp
                 "Display Sheet Info", displayCurProj
                 "Next Test Error", fun _ _ _ _ -> printf "Next Error:" // Go to the nexterror in a test
             ]

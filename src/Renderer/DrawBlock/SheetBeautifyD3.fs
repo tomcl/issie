8b8e9582437b07e5fc501b5bf677d6fb80ff53d2
Vidi-M <116262063+Vidi-M@users.noreply.github.com>
2024-03-20 21:42:38 +0000

    implemented using list.fold instead

--- src/Renderer/DrawBlock/SheetBeautifyD3.fs
+++ src/Renderer/DrawBlock/SheetBeautifyD3.fs
@@ -13,6 +13,7 @@ open BlockHelpers
 open Symbol
 open BusWireRoute
 open BusWire
+open BusWireUpdateHelpers
 open SheetBeautifyHelpers
 open EEExtensions
 open SheetBeautifyD2
@@ -76,16 +77,20 @@ let wireLabelPositions (outputID: OutputPortId) (wires: Wire list) (model: BusWi
 
     outputPos :: inputPosList
 
-//--------------------------------------------------------------------------------------//
-//                                D3                                                    //
-//--------------------------------------------------------------------------------------//
 
-// Very initial start to D3, currently filters wires on a sheet into long and short wires
-
-// TODOS:
-// - Find a way of replacing longWires with labels (e.g using AddNotConnected).
-// - Figure out where to position the label.
-// - What names to give each label.
+    
+    
+    
+    //--------------------------------------------------------------------------------------//
+    //                                D3                                                    //
+    //--------------------------------------------------------------------------------------//
+    
+    // Very initial start to D3, currently filters wires on a sheet into long and short wires
+    
+    // TODOS:
+    // - Find a way of replacing longWires with labels (e.g using AddNotConnected).
+    // - Figure out where to position the label.
+    // - What names to give each label.
 let sheetWireLabelSymbol (model: SheetT.Model) : SheetT.Model =
     let wires = getAllWires model
     let wireLengths = List.map getWireLength wires
@@ -95,14 +100,47 @@ let sheetWireLabelSymbol (model: SheetT.Model) : SheetT.Model =
         |> List.map fst // Remove the length from snd as Wire list is now filtered
     let OutputPortIds = longWires |> getOutputPortId |> List.distinct
     let gap: XYPos = { X = 10; Y = 0 }
-    for outputPort in OutputPortIds do
-        let posList = wireLabelPositions outputPort wires model.Wire
-        let outputSymModel, outputSymId =
-            SymbolUpdate.addSymbol [] (model.Wire.Symbol) posList.Head IOLabel "I1"
-        for pos in posList.Tail do
-            let inputSymModel, inputSymId =
-                SymbolUpdate.addSymbol [] (model.Wire.Symbol) pos IOLabel "I1"
+        // for outputPort in OutputPortIds do
+        // let posList = wireLabelPositions outputPort wires model.Wire
+        // let outputSymModel, outputSymId =
+        //     SymbolUpdate.addSymbol [] (model.Wire.Symbol) posList.Head IOLabel "I1"
+        //     for pos in posList.Tail do
+        //     let inputSymModel, inputSymId =
+        //         SymbolUpdate.addSymbol [] (model.Wire.Symbol) pos IOLabel "I1"
+                
+                
+        //         model |> Optic.set symbolModel_ outputSymModel
+        //         model
 
-            model |> Optic.set symbolModel_ inputSymModel
-    model
 
+    let updatedModel: SheetT.Model = 
+        OutputPortIds
+        // Accumulator is currentModel. We repeat this List.fold for each outputPort
+        //|> List.fold (fun currentModel outputPort -> // make my own delete wires function
+            //BusWireUpdateHelpers.deleteWiresWithPort outputPort currentModel.Wire) model
+        |> List.fold (fun currentModel outputPort ->
+            let posList = wireLabelPositions outputPort wires currentModel.Wire
+
+            // add the IOLabel for the output symbol to the SymbolT.Model
+            let (outputSymModel: SymbolT.Model), outputSymId =
+                SymbolUpdate.addSymbol [] (currentModel.Wire.Symbol) posList.Head IOLabel "I1"
+            // create a new SheetT.Model with the output symbol added
+            let (modelWithOutputSym: SheetT.Model) = currentModel |> Optic.set SheetT.symbol_ outputSymModel
+
+            // take the postList.Tail list of XYPos and add the IOLabel for each symbol to the SymbolT.Model
+            posList.Tail
+            // Accumulator is currentModelWithOutputSym. Smaller inside loop, we repeat this List.fold for each inputPort pos
+            |> List.fold (fun currentModelAddingInputSyms pos ->
+                let inputSymModel, inputSymId =
+                    SymbolUpdate.addSymbol [] (currentModelAddingInputSyms.Wire.Symbol) pos IOLabel "I1"
+                currentModelAddingInputSyms |> Optic.set SheetT.symbol_ inputSymModel
+            ) modelWithOutputSym // feed modelWithOutputSym as our initial starting point (aka default value)
+
+        ) model // feed model as our initial starting point (aka default value) for the outer loop
+        // return updatedModel
+        
+    updatedModel
+    
+    
+                
+                

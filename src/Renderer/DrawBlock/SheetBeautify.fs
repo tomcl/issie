346927dc5de4e3f84b4b9b1f27a61931e6e1a45a
Sohailul Islam Alvi <94545356+alvi-codes@users.noreply.github.com>
2024-03-22 20:00:41 +0000

    Update SheetBeautify.fs

--- src/Renderer/DrawBlock/SheetBeautify.fs
+++ src/Renderer/DrawBlock/SheetBeautify.fs
@@ -9,7 +9,7 @@ Whole sheet functions are normally applied to whole sheet. In many cases a featu
 apply them to currently selected wires or components. That provides users control over what gets beautified.
 Ideal beautify code will never make things worse so can be applied to whole sheet always.
 
-Otehr relevant modules:
+Other relevant modules:
 SheetBeautifyHelpers (helpers)
 Generatedata (used to make randomized tests)
 TestDrawBlock (used to test the code written here).
@@ -36,21 +36,16 @@ open SheetBeautifyD3
 
 let beautifySheet (initialModel : SheetT.Model) : SheetT.Model =
 
-
+    // Adding current model to the undo/redo list
     let newUndoList, newRedoList = SheetUpdateHelpers.appendUndoList initialModel.UndoList initialModel, initialModel.RedoList
 
     let modelToUpdate = {initialModel with UndoList = newUndoList; RedoList = newRedoList}
 
+    // Calling all the beautify functions in order: D2, D1 and D3
     modelToUpdate
     |> SheetBeautifyD2.sheetOrderFlip
     |> SheetBeautifyD1.sheetAlignScale
     |> SheetBeautifyD3.sheetWireLabelSymbol
-
-
-
-
-
-
 
 
 /// constants used by SheetBeautify

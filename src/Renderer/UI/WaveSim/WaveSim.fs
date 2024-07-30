67fc19cdee939421c6e87cb35258ad1299122422
Samuel Wang <samuel.wang21@imperial.ac.uk>
2024-07-30 17:54:58 +0100

    fix: broken scrollbar button when just one cycle is displayed
    (cherry picked from commit 33465b717a7e27dbccb74259426b432ee2e0d6ef)

--- src/Renderer/UI/WaveSim/WaveSim.fs
+++ src/Renderer/UI/WaveSim/WaveSim.fs
@@ -996,7 +996,7 @@ let makeScrollbar (wsm: WaveSimModel) (dispatch: Msg->unit): ReactElement =
 
     div [Style[ MarginTop "16px"; MarginBottom "16px"; Height "30px"]] [
         button [ Button.Props [clkCycleLeftStyle] ]
-            (fun _ -> scrollWaveformsBy (-wsm.ShownCycles+1))
+            (fun _ -> scrollWaveformsBy (-wsm.ShownCycles))
             (str "◀")
         svg
             [Style [Width $"{bkgWidth}"; Height $"{WaveSimStyle.Constants.scrollbarHeight}px"];]
@@ -1007,7 +1007,7 @@ let makeScrollbar (wsm: WaveSimModel) (dispatch: Msg->unit): ReactElement =
                 rect (tbBufferRightPropList wsm.ScrollbarTbPos wsm.ScrollbarTbWidth) []; // right buffer
             ]
         button [ Button.Props [scrollbarClkCycleRightStyle]]
-            (fun _ -> scrollWaveformsBy (wsm.ShownCycles-1))
+            (fun _ -> scrollWaveformsBy (wsm.ShownCycles))
             (str "▶")
     ]
 

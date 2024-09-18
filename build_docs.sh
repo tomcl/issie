86558df179215fe3796abda91d1b9b04feeff105
Tom Clarke <tomcl@users.noreply.github.com>
2024-09-18 12:09:00 +0100

    update docs build

--- build_docs.sh
+++ build_docs.sh
@@ -2,8 +2,8 @@
 dotnet build
 
 # Define the directories containing the XML files
-MAIN_XML="src/Main/bin/Debug/net7.0/Main.xml"
-RENDERER_XML="src/Renderer/bin/Debug/net7.0/Renderer.xml"
+MAIN_XML="src/Main/bin/Debug/net8.0/Main.xml"
+RENDERER_XML="src/Renderer/bin/Debug/net8.0/Renderer.xml"
 
 # Use sed to replace all occurrences of <> with &lt;&gt; in the XML files
 sed -i.bak 's/<>/\&lt;\&gt;/g' "$MAIN_XML"

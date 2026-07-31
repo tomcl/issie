c1ea034a283ac94f11632431dc78991b94a4211e
tomcl <tomcl@users.noreply.github.com>
2026-07-31 14:26:50 +0000

    deploy: c78cd1e6bbb8dc63a19190eafa22f895b8f9b953

--- content/fsdocs-details-set-expanded.js
+++
@@ -1,9 +0,0 @@
-const expandDetails = !!JSON.parse(localStorage.getItem('details-expanded'));
-
-addEventListener('load', _ => {
-    if (expandDetails) {
-        for (const details of document.getElementsByTagName('details')) {
-            details.setAttribute('open', 'true');
-        }
-    }
-});
